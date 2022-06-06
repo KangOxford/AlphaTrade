86d4472da5ebc66fd838bf7bb1d22e00e0a13663
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-06 07:40:21 +0100

    gan

--- GANs/SimpleGAN/SimpleGAN.py
+++ GANs/SimpleGAN/SimpleGAN.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -11,14 +12,18 @@ class Discriminator(nn.Module):
     def __init__(self, input):
         super.__init__()
         self.discriminator = nn.Sequential()
-        self.discriminator.add(nn.Linear(input, 256))
-        self.discriminator.add(nn.LeakyReLU(0.01))
-        self.discriminator.add(nn.Linear(256,1))
-        self.discriminator.add(nn.Tanh())
+        self.discriminator.add(nn.Linear(input, 256), nn.LeakyReLU(0.01))
+        self.discriminator.add(nn.Linear(256,1)     , nn.Sigmoid())
     def forward(self, input):
         return self.discriminator(input)
 
 class Generator(nn.Module):
     def __init__(self, latent_dim, generated_dim):
         super.__init__()
+        self.generator = nn.Sequential()
+        self.generator.add(nn.linear(latent_dim, 256),    nn.LeakyReLU(0.01))
+        self.generator.add(nn.linear(256, generated_dim), nn.Tanh)
+    def forward(self, input_lanten, input_generate):
+        return self.generator(input_lanten, input_generate)
+
 
---
+++ GANs/SimpleGAN/SimpleGAN_train.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from SimpleGAN import Discriminator, Generator
+
+# >>> 01 Iintailize Hyperparameters <<<
+device = "cuda" if torch.cuda.is_available() else "cpu"
+lr = 3e-4
+latent_dim = 64
+generated_dim = 28*28
+batch_size = 32
+num_epochs = 100
+
+# >>> 02 Iintailize Generator and Discriminator <<<
+discriminator = Discriminator(generated_dim).to(device)
+generator = Generator(latent_dim, generated_dim).to(device)
+
+# >>> 03 Iintailize Dataset <<<
+fixed_noise = torch.randn((batch_size, latent_dim)).to(device)
+transforms = transforms.Compose([transforms.ToTensor, transforms.Normalize((),())])
+dataset = datasets.MNIST(root="./dataset/", train=False, transform=transforms, download=True)
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# >>> 04 Iintailize Optimizer <<<
+optim_discriminator =  optim.Adam(discriminator.parameters(), lr = lr)
+optim_generator = optim.Adam(generator.parameters(), lr = lr)
+criterion = nn.BCELoss()
+
+# >>> 05 Set Tensorboard <<<
+writer_gene = SummaryWriter(f"logs/gene")
+writer_data = SummaryWriter(f"logs/data")
+step = 0 
+
+# >>> 06 Training Loops <<<
+for epoch in range(num_epochs):
+    for batch_index, (data, data_lable) in enumerate(loader):
+        data = data.view(-1, 784).to(device)
+        batch_size = data.shape[0]
+        # >>> 06.01 Training Loops / Training Discriminator <<<
+        generated = generator(torch.randn(batch_size, latent_dim).to(device))
+        
+
+
+
