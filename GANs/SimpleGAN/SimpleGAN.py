371d0eb98203efb374c6a086e628bd8a1a6d1e25
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-06 08:43:27 +0100

    gan

--- GANs/SimpleGAN/SimpleGAN.py
+++ GANs/SimpleGAN/SimpleGAN.py
@@ -10,20 +10,20 @@ from torch.utils.tensorboard import SummaryWriter
 
 class Discriminator(nn.Module):
     def __init__(self, input):
-        super.__init__()
-        self.discriminator = nn.Sequential()
-        self.discriminator.add(nn.Linear(input, 256), nn.LeakyReLU(0.01))
-        self.discriminator.add(nn.Linear(256,1)     , nn.Sigmoid())
+        super(Discriminator, self).__init__()
+        self.discriminator = nn.Sequential(
+            nn.Linear(input, 256), nn.LeakyReLU(0.01),
+            nn.Linear(256,1)     , nn.Sigmoid()
+        )
     def forward(self, input):
         return self.discriminator(input)
 
 class Generator(nn.Module):
     def __init__(self, latent_dim, generated_dim):
-        super.__init__()
-        self.generator = nn.Sequential()
-        self.generator.add(nn.linear(latent_dim, 256),    nn.LeakyReLU(0.01))
-        self.generator.add(nn.linear(256, generated_dim), nn.Tanh)
+        super(Generator, self).__init__()
+        self.generator = nn.Sequential(
+            nn.Linear(latent_dim, 256),    nn.LeakyReLU(0.01),
+            nn.Linear(256, generated_dim), nn.Tanh()
+        )
     def forward(self, input_lanten, input_generate):
         return self.generator(input_lanten, input_generate)
-
-
