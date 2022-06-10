61c0fe32eeb4a42db27e6bb2d7af52ca8c7c296d
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-10 06:03:54 +0100

    new

---
+++ GANs/WGAN-GP/Grandient_Penalty.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+def gradient_penalty(generator, real, fake):
+    
