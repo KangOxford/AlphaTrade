f8dc8f4e3982ea24fca60bd0a5e2a8871ed9594a
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-11 08:33:21 +0100

    ACGAN

---
+++ GANs/ACGAN/ACGAN_Train.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from ArgumentParser import Argument_Parser
+from ConditionalGAN import Generator, Discriminator
+
+# ---------------------------------------------- #
+#                                                #
+#    ACGAN May.2022                              #
+#    Written by Kang                             #
+#    Funtion:                                    #
+#      For the training process of the ACGAN     #
+#    Dependency:                                 #
+#      Discriminator, Generator in ACGAN         #
+#      Argument_Parser in ArgumentParser         #
+#                                                #
+# ---------------------------------------------- #
+
+# >>> 01 Iintailize ArgumentParser <<<
+option = Argument_Parser()
+
+# >>> 02 Iintailize Generator and Discriminator <<<
+def initializing_weights(model):
+    nn.init.normal_(model.weight.data, 0.0, 0.01)
+
+
+# discriminator = Discriminator(channels_dim, discriminator_dim).to(device)
+# generator = Generator(latent_dim, channels_dim, generated_dim).to(device)
+# initializing_weights(discriminator)
+# initializing_weights(generator)
+
+
+
+
+
