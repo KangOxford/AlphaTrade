5ecaa54771ed2024af97d8ec1b1fde53ec08b969
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-11 05:51:38 +0100

    Update ConditionalGAN.py

--- GANs/ConditionalGAN/ConditionalGAN.py
+++ GANs/ConditionalGAN/ConditionalGAN.py
@@ -11,3 +11,5 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--epochs_number", type = int, default = 100)
+parser.add_argument("--batch_size", type = int, default = 32) #default for most of the choices.
