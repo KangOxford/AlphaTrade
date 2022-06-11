5730d59efc5e53f7f8faa903e22eef5e3306e65e
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-11 08:08:36 +0100

    Update ConditionalGAN_Train.py

--- GANs/ConditionalGAN/ConditionalGAN_Train.py
+++ GANs/ConditionalGAN/ConditionalGAN_Train.py
@@ -36,5 +36,21 @@ parser.add_argument("--beta_second_moment", type = float, default = 0.99)
 parser.add_argument("--latent_dim", type = int, default = 64)
 parser.add_argument("--generated_dim", type = int, default = 32)
 parser.add_argument("--channel_dim", type = int, default = 3)
+# >>> 01.04 Parsing Parameters <<<
+(options, args) = parser.parse_args()
+print(options, args)
+
+# >>> 02 Iintailize Generator and Discriminator <<<
+def initializing_weights(model):
+    nn.init.normal_(model.weight.data, 0.0, 0.01)
+
+
+# discriminator = Discriminator(channels_dim, discriminator_dim).to(device)
+# generator = Generator(latent_dim, channels_dim, generated_dim).to(device)
+# initializing_weights(discriminator)
+# initializing_weights(generator)
+
+
+
 
 
