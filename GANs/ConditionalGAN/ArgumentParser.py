5ffaff9bc52fd6cc5a7cbacc07f85704ae837bc3
KangOxford <kang.li@maths.ox.ac.uk>
2022-06-11 08:22:10 +0100

    Update ArgumentParser.py

--- GANs/ConditionalGAN/ArgumentParser.py
+++ GANs/ConditionalGAN/ArgumentParser.py
@@ -9,7 +9,7 @@ parser.add_argument("--batch_size", type = int, default = 32) # the default for 
 parser.add_argument("--learning_rate", type = float, default = 0.01)
 parser.add_argument("--beta_firt_moment", type = float, default = 0.5)
 parser.add_argument("--beta_second_moment", type = float, default = 0.99)
-# >>> 01.03 Iintailize Generator Parameters <<<
+# >>> 01.03 Iintailize Network Parameters <<<
 parser.add_argument("--latent_dim", type = int, default = 64)
 parser.add_argument("--generated_dim", type = int, default = 32)
 parser.add_argument("--channel_dim", type = int, default = 3)
