50f4cfc7f7cd232169d17895a909a9c7f7938b9e
KangOxford <kang.li@maths.ox.ac.uk>
2022-08-23 17:26:24 +0100

    v27.1

--- gym_trading/train/train_gym_trading.py
+++ gym_trading/train/train_gym_trading.py
@@ -5,6 +5,7 @@ import warnings
 import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
 from gym_trading.envs.base_environment import BaseEnv
 from gym_trading.data.data_pipeline import ExternalData
@@ -12,13 +13,17 @@ warnings.filterwarnings("ignore")
 Flow = ExternalData.get_sample_order_book_data()
 num_cpu = 10 
 # env = gym.make("GymTrading-v1",Flow = Flow) ## TODO
-env = DummyVecEnv([lambda: gym.make("GymTrading-v1",Flow = Flow)] * num_cpu)
-check_env(env)
-
-model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="./ppo_gymtrading_tensorboard8/")
+venv = DummyVecEnv([lambda: gym.make("GymTrading-v1",Flow = Flow)] * num_cpu)
+monitor_venv = Monitor(DummyVecEnv([lambda: gym.make("GymTrading-v1",Flow = Flow)] * num_cpu))
+# check_env(env)
+# %%
+model = PPO("MultiInputPolicy", 
+            venv, 
+            verbose=1, 
+            tensorboard_log="/Users/kang/GitHub/NeuralLOB/ppo_gymtrading_tensorboard8/")
 # %time model.learn(total_timesteps=int(1e7), n_eval_episodes = int(1e5))
 # model.learn(total_timesteps=int(3e6), n_eval_episodes = int(1e5))
-model.learn(total_timesteps=int(1e10), tb_log_name="short_horizon")
+model.learn(total_timesteps=int(1e10), tb_log_name="Short_Horizon")
 model.save("gym_trading-v1") 
 
 
