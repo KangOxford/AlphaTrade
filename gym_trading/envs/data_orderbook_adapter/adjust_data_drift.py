637d9e2160df29f4a227edb61e06b4b99fb5ee31
KangOxford <kang.li@maths.ox.ac.uk>
2022-10-18 19:53:42 +0100

    refactor

---
+++ gym_trading/envs/data_orderbook_adapter/adjust_data_drift.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from gym_trading.envs.data_orderbook_adapter import utils
+
+
+class DataAdjuster():
+    def __init__(self, d2):
+        self.adjust_data_drift_id = 10000
+        self.d2 = d2
+
+    def adjust_data_drift(self, order_book, timestamp, index):
+            
+        print(order_book)#tbd
+        my_list, right_list = utils.get_two_list4compare(order_book, index, self.d2)
+        my_array, right_array = np.array(my_list), np.array(right_list)
+        print("my_list")
+        print(my_list)
+        print("right_list")
+        print(right_list)
+        
+        right_order = list(set(right_list) - set(my_list))
+        wrong_order = list(set(my_list) -set(right_list))
+        if len(right_order) == 0 and len(wrong_order) == 0:
+            print("no data drift: no incomming new limit order outside the 10 price levels")
+            message = None
+        else:
+            # global adjust_data_drift_id
+            # if  np.sum(my_array != right_array) == 2:
+            if len(right_order) == 1 and len(wrong_order) == 1:
+                if my_list[-2] == right_list[-2] :
+                    price = right_list[-2]
+                    if right_order[0] == wrong_order[0]:
+                        raise NotImplementedError
+                    elif right_order[0] > wrong_order[0]:
+                        quantity = right_order[0] - wrong_order[0]
+                        side = 'bid'
+                    elif right_order[0] < wrong_order[0]:
+                        quantity = -right_order[0] + wrong_order[0]
+                        # side = 'ask' # cancel order here
+                        # return cancel_by_price(order_book, price)
+                        order_list =  order_book.bids.get_price_list(price)
+                        
+                        #search quanity
+                        for order in order_list:
+                            if order.quantity == quantity:
+                                order_book.cancel_order(side = 'bid', 
+                                                        order_id = order.order_id,
+                                                        time = order.timestamp, 
+                                                        )
+                                return order_book # here just cancel single order, not combined order
+                            
+                        raise NotImplementedError
+                elif my_list[-1] == right_list[-1]:
+                    price = right_list[-2]
+                    quantity = right_list[-1]
+                    side = 'bid'
+                elif my_list[-1] != right_list[-1] and  my_list[-2] != right_list[-2]:
+                    # two actions needs to be taken in this step
+                    pass
+                else:
+                    raise NotImplementedError
+                self.adjust_data_drift_id += 1
+                trade_id = self.adjust_data_drift_id
+                order_id = self.adjust_data_drift_id
+                
+                str_int_timestamp = str(int(timestamp[0:5]) * int(1e9) + (int(timestamp[6:15]) +1))
+                timestamp = str(str_int_timestamp[0:5])+'.'+str(str_int_timestamp[5:15])
+                
+                message = {'type': 'limit','side': side,'quantity': quantity,'price': price,'trade_id': trade_id, "timestamp":timestamp, 'order_id':order_id}
+                print('\n'+'-'*15)
+                print(">>> ADJUSTED <<<")
+                print('-'*15+'\n')   
+            elif len(right_order) == 2 and len(wrong_order) == 2:
+                right_order_price =  right_list[-2]
+                wrong_order_price =  my_list[-2]
+                if right_order_price > wrong_order_price: # just insert new order
+                    side = 'bid'
+                    price = right_order[0]
+                    quantity = right_order[1]
+                    self.adjust_data_drift_id += 1
+                    trade_id = self.adjust_data_drift_id
+                    order_id = self.adjust_data_drift_id
+                    
+                    str_int_timestamp = str(int(timestamp[0:5]) * int(1e9) + (int(timestamp[6:15]) +1))
+                    timestamp = str(str_int_timestamp[0:5])+'.'+str(str_int_timestamp[5:15])
+                    
+                    message = {'type': 'limit','side': side,'quantity': quantity,'price': price,'trade_id': trade_id, "timestamp":timestamp, 'order_id':order_id}
+                    print('\n'+'-'*15)
+                    print(">>> ADJUSTED <<<")
+                    print('-'*15+'\n')
+                elif right_order_price < wrong_order_price:
+                    # wrong order been cancelled outside the order book 
+                    order_book = utils.partly_cancel(order_book, right_order_price, wrong_order_price)
+                    message = None
+                else: 
+                    raise NotImplementedError
+            elif np.sum(my_array != right_array) == 2:
+                if right_array[-2] >  my_array[-2]:
+                    side = 'bid'
+                    price = right_array[-2]
+                    quantity = right_array[-1]
+                    self.adjust_data_drift_id += 1
+                    trade_id = self.adjust_data_drift_id
+                    order_id = self.adjust_data_drift_id
+                    
+                    str_int_timestamp = str(int(timestamp[0:5]) * int(1e9) + (int(timestamp[6:15]) +1))
+                    timestamp = str(str_int_timestamp[0:5])+'.'+str(str_int_timestamp[5:15])
+                    
+                    message = {'type': 'limit','side': side,'quantity': quantity,'price': price,'trade_id': trade_id, "timestamp":timestamp, 'order_id':order_id}
+                    print('\n'+'-'*15)
+                    print(">>> ADJUSTED <<<")
+                    print('-'*15+'\n')
+                elif right_array[-2] <  my_array[-2]:
+                    # =============================================================================
+                    # part of order_list at this price has been partly cancelled outside the order book              
+                    # ----------------------------------------------------------------------------
+                    # my_list
+                    # [31209700, 210, 31204400, 1, 31203500, 100, 31201000, 8, 31200500, 1, 
+                    # 31200000, 4, 31194000, 8, 31187600, 8, 31187400, 13, 31187200, 10]
+                    # ----------------------------------------------------------------------------
+                    # right_list
+                    # [31209700, 210, 31204400, 1, 31203500, 100, 31201000, 8, 31200500, 1, 
+                    # 31200000, 4, 31194000, 8, 31187600, 8, 31187400, 13, 31187100, 1]
+                    # =============================================================================
+                    right_order_price =  right_list[-2]
+                    wrong_order_price =  my_list[-2]
+                    order_book = utils.partly_cancel(order_book, right_order_price, wrong_order_price)
+                    message = None
+                else: raise NotImplementedError
+            elif np.sum(my_array != right_array) == 1:
+                # =============================================================================
+                # part of order_list at this price has been partly cancelled outside the order book
+                # Quantity    20  |  Price 31155100  |  Trade_ID   15227277  |  Time 34200.290719105
+                # Quantity     1  |  Price 31155100  |  Trade_ID      10003  |  Time 34204.721258569
+                # ----------------------------------------------------------------------------
+                # my_list
+                # [31171400, 5, 31171000, 200, 31167100, 4, 31160000, 4, 31159800, 20, 
+                #  31158100, 10, 31158000, 7, 31157700, 1, 31155500, 70, 31155100, 21]
+                # ----------------------------------------------------------------------------
+                # right_list
+                # [31171400, 5, 31171000, 200, 31167100, 4, 31160000, 4, 31159800, 20, 
+                #  31158100, 10, 31158000, 7, 31157700, 1, 31155500, 70, 31155100, 1]
+                # =============================================================================
+                price = right_array[-2]
+                quantity = right_array[-1] # right quantity
+                quantity_list = [] # wrong quantity list
+                order_id_list = []
+                timestamp_list = []
+                for item in order_book.bids.get_price_list(price):
+                    quantity_list.append(item.quantity)
+                    order_id_list.append(item.order_id)
+                    timestamp_list.append(item.timestamp)
+                try:quantity_index = quantity_list.index(quantity)
+                except: raise NotImplementedError
+                difference_index = np.array([i for i in range(len(quantity_list)) if i != quantity_index])
+                if index == 4: breakpoint()
+                returned_timestamp_array = np.array(timestamp_list)[difference_index]
+                returned_order_id_array = np.array(order_id_list)[difference_index] # returned_order_id_array for cancel order
+                assert len(returned_order_id_array) == 1, "NotImplemented, only implement the single order situation"
+                order_book.cancel_order(side = 'bid', 
+                                        order_id = returned_order_id_array[0],
+                                        time = returned_timestamp_array[0], 
+                                        )
+                message = None
+                print('\n'+'-'*15)
+                print(">>> ADJUSTED <<<")
+                print('-'*15+'\n')
+            else:
+                raise NotImplementedError
+        if message is not None:
+            trades, order_in_book = order_book.process_order(message, True, False)
+        print(order_book) # tbd
+        return order_book
+        # print("Adjusted Order Book")
+        # print(brief_order_book(order_book))
+    # my_list[~my_list.apply(tuple,1).isin(right_list.apply(tuple,1))]
+    # right_list[~right_list.apply(tuple,1).isin(my_list.apply(tuple,1))]
+    # my_list is right_list
+    
+    # =============================================================================
--- gym_trading/envs/data_orderbook_adapter/level_n_adapter.py
+++ gym_trading/envs/data_orderbook_adapter/level_n_adapter.py
@@ -1,202 +1,20 @@
 # -*- coding: utf-8 -*-
 
+price_level = 10 # wont be changed during running
+# =============================================================================
+# 01 IMPORT PACKAGES
 # =============================================================================
 import numpy as np
-adjust_data_drift_id = 10000
-price_level = 10 # wont be changed during running
-t3_count = 0
-
-# =============================================================================
-# Price = 31155000
-def cancel_by_price(order_book, Price):
-    side = 'bid'
-    order_list =  order_book.bids.get_price_list(Price)
-    order = order_list.get_head_order()
-    order_id = order.order_id
-    trade_id = order.trade_id
-    timestamp = order.timestamp
-    order_book.cancel_order(side, trade_id, time = timestamp)
-    return order_book
-
-
-def get_two_list4compare(order_book, index):
-    global price_level
-    my_list = brief_order_book(order_book)[0:2*price_level]
-    right_list = d2.iloc[index,:].reset_index().drop(['index'],axis= 1).iloc[:,0].to_list() 
-    return my_list, right_list
-    
-def is_right_answer(order_book, index):
-    my_list, right_list = get_two_list4compare(order_book, index)
-    return len(list(set(right_list) - set(my_list))) == 0
-    
-    
-def brief_order_book(order_book):
-    my_list = []
-    count = 0
-    global  price_level
-    for key, value in reversed(order_book.bids.price_map.items()):
-        count +=1 
-        quantity = value.volume
-        price = value.head_order.price
-        my_list.append(price)
-        my_list.append(quantity)
-        if count == price_level:
-            break
-    return my_list
-
-def adjust_data_drift(order_book, timestamp, index):
-        
-    print(order_book)#tbd
-    my_list, right_list = get_two_list4compare(order_book, index)
-    my_array, right_array = np.array(my_list), np.array(right_list)
-    print("my_list")
-    print(my_list)
-    print("right_list")
-    print(right_list)
-    
-    right_order = list(set(right_list) - set(my_list))
-    wrong_order = list(set(my_list) -set(right_list))
-    if len(right_order) == 0 and len(wrong_order) == 0:
-        print("no data drift: no incomming new limit order outside the 10 price levels")
-        message = None
-    else:
-        global adjust_data_drift_id
-        if len(right_order) == 1 and len(wrong_order) == 1:
-            if my_list[-2] == right_list[-2] :
-                price = right_list[-2]
-                if right_order[0] == wrong_order[0]:
-                    raise NotImplementedError
-                elif right_order[0] > wrong_order[0]:
-                    quantity = right_order[0] - wrong_order[0]
-                    side = 'bid'
-                elif right_order[0] < wrong_order[0]:
-                    quantity = -right_order[0] + wrong_order[0]
-                    # side = 'ask' # cancel order here
-                    return cancel_by_price(order_book, price)
-            elif my_list[-1] == right_list[-1]:
-                price = right_list[-2]
-                quantity = right_list[-1]
-                side = 'bid'
-            else:
-                raise NotImplementedError
-            adjust_data_drift_id += 1
-            trade_id = adjust_data_drift_id
-            order_id = adjust_data_drift_id
-            
-            str_int_timestamp = str(int(timestamp[0:5]) * int(1e9) + (int(timestamp[6:15]) +1))
-            timestamp = str(str_int_timestamp[0:5])+'.'+str(str_int_timestamp[5:15])
-            
-            message = {'type': 'limit','side': side,'quantity': quantity,'price': price,'trade_id': trade_id, "timestamp":timestamp, 'order_id':order_id}
-            print('\n'+'-'*15)
-            print(">>> ADJUSTED <<<")
-            print('-'*15+'\n')   
-        elif len(right_order) == 2 and len(wrong_order) == 2:
-            right_order_price =  right_list[-2]
-            wrong_order_price =  my_list[-2]
-            if right_order_price > wrong_order_price: # just insert new order
-                side = 'bid'
-                price = right_order[0]
-                quantity = right_order[1]
-                adjust_data_drift_id += 1
-                trade_id = adjust_data_drift_id
-                order_id = adjust_data_drift_id
-                
-                str_int_timestamp = str(int(timestamp[0:5]) * int(1e9) + (int(timestamp[6:15]) +1))
-                timestamp = str(str_int_timestamp[0:5])+'.'+str(str_int_timestamp[5:15])
-                
-                message = {'type': 'limit','side': side,'quantity': quantity,'price': price,'trade_id': trade_id, "timestamp":timestamp, 'order_id':order_id}
-                print('\n'+'-'*15)
-                print(">>> ADJUSTED <<<")
-                print('-'*15+'\n')
-            elif right_order_price < wrong_order_price:
-                # wrong order been cancelled outside the order book 
-                for price, order_list in reversed(order_book.bids.price_map.items()):
-                    print(right_order_price, price , wrong_order_price)
-                    if right_order_price < price  and price <= wrong_order_price:
-                        for order in order_list:
-                            order_book.cancel_order(side = 'bid', 
-                                                    order_id = order.order_id,
-                                                    time = order.timestamp, 
-                                                    )
-                message = None
-            else: 
-                raise NotImplementedError
-        elif np.sum(my_array != right_array) == 2:
-            side = 'bid'
-            price = right_array[-2]
-            quantity = right_array[-1]
-            adjust_data_drift_id += 1
-            trade_id = adjust_data_drift_id
-            order_id = adjust_data_drift_id
-            
-            str_int_timestamp = str(int(timestamp[0:5]) * int(1e9) + (int(timestamp[6:15]) +1))
-            timestamp = str(str_int_timestamp[0:5])+'.'+str(str_int_timestamp[5:15])
-            
-            message = {'type': 'limit','side': side,'quantity': quantity,'price': price,'trade_id': trade_id, "timestamp":timestamp, 'order_id':order_id}
-            print('\n'+'-'*15)
-            print(">>> ADJUSTED <<<")
-            print('-'*15+'\n')
-        elif np.sum(my_array != right_array) == 1:
-            # =============================================================================
-            # part of order_list at this price has been partly cancelled outside the order book
-            # Quantity    20  |  Price 31155100  |  Trade_ID   15227277  |  Time 34200.290719105
-            # Quantity     1  |  Price 31155100  |  Trade_ID      10003  |  Time 34204.721258569
-            # ----------------------------------------------------------------------------
-            # my_list
-            # [31171400, 5, 31171000, 200, 31167100, 4, 31160000, 4, 31159800, 20, 
-            #  31158100, 10, 31158000, 7, 31157700, 1, 31155500, 70, 31155100, 21]
-            # ----------------------------------------------------------------------------
-            # right_list
-            # [31171400, 5, 31171000, 200, 31167100, 4, 31160000, 4, 31159800, 20, 
-            #  31158100, 10, 31158000, 7, 31157700, 1, 31155500, 70, 31155100, 1]
-            # =============================================================================
-            price = right_array[-2]
-            quantity = right_array[-1] # right quantity
-            quantity_list = [] # wrong quantity list
-            order_id_list = []
-            timestamp_list = []
-            for item in order_book.bids.get_price_list(price):
-                quantity_list.append(item.quantity)
-                order_id_list.append(item.order_id)
-                timestamp_list.append(item.timestamp)
-            try:quantity_index = quantity_list.index(quantity)
-            except: raise NotImplementedError
-            difference_index = np.array([i for i in range(len(quantity_list)) if i != quantity_index])
-            if index == 4: breakpoint()
-            returned_timestamp_array = np.array(timestamp_list)[difference_index]
-            returned_order_id_array = np.array(order_id_list)[difference_index] # returned_order_id_array for cancel order
-            assert len(returned_order_id_array) == 1, "NotImplemented, only implement the single order situation"
-            order_book.cancel_order(side = 'bid', 
-                                    order_id = returned_order_id_array[0],
-                                    time = returned_timestamp_array[0], 
-                                    )
-            message = None
-            print('\n'+'-'*15)
-            print(">>> ADJUSTED <<<")
-            print('-'*15+'\n')
-        else:
-            raise NotImplementedError
-    if message is not None:
-        trades, order_in_book = order_book.process_order(message, True, False)
-    print(order_book) # tbd
-    return order_book
-    # print("Adjusted Order Book")
-    # print(brief_order_book(order_book))
-# my_list[~my_list.apply(tuple,1).isin(right_list.apply(tuple,1))]
-# right_list[~right_list.apply(tuple,1).isin(my_list.apply(tuple,1))]
-# my_list is right_list
-
-# =============================================================================
-
-
-# =============================================================================
-# #tbd
-
 import pandas as pd
-df2 = pd.read_csv("/Users/kang/Data/AMZN_2021-04-01_34200000_57600000_orderbook_10.csv", header = None)
-
+from gym_trading.envs.data_orderbook_adapter import utils
+from gym_trading.envs.data_orderbook_adapter.adjust_data_drift import DataAdjuster
 from gym_trading.envs.orderbook import OrderBook
 order_book = OrderBook()
+
+# =============================================================================
+# 02 READ DATA
+# =============================================================================
+df2 = pd.read_csv("/Users/kang/Data/AMZN_2021-04-01_34200000_57600000_orderbook_10.csv", header = None)
 
 l1 = df2.iloc[0,:]
 column_numbers=[i for i in range(price_level * 4) if i%4==2 or i%4==3]
@@ -206,6 +24,15 @@ l2 = l2.reset_index().drop(['index'],axis = 1)
 # =============================================================================
 column_numbers=[i for i in range(price_level * 4) if i%4==2 or i%4==3]
 d2 = df2.iloc[:,column_numbers]  
+# =============================================================================
+
+
+df = pd.read_csv("/Users/kang/Data/AMZN_2021-04-01_34200000_57600000_message_10.csv", header=None)
+df.columns = ["timestamp",'type','order_id','quantity','price','side','remark']
+df["timestamp"] = df["timestamp"].astype(str)
+
+# =============================================================================
+# 03 CONFIGURARION OF ORDERBOOK
 # =============================================================================
 
 limit_orders = []
@@ -230,16 +57,6 @@ for order in limit_orders:
 # The current book may be viewed using a print
 print(order_book)
 
-# #tbd
-# =============================================================================
-
-# =============================================================================
-# # tbd
-
-import pandas as pd
-df = pd.read_csv("/Users/kang/Data/AMZN_2021-04-01_34200000_57600000_message_10.csv", header=None)
-df.columns = ["timestamp",'type','order_id','quantity','price','side','remark']
-df["timestamp"] = df["timestamp"].astype(str)
 # size = 50 # pass
 # size = 75 # pass
 # size = 85 # pass
@@ -259,8 +76,16 @@ df["timestamp"] = df["timestamp"].astype(str)
 # size = 2190 # pass
 # size = 2199 # pass
 # size = 2900 # pass type3 cancel
-# size = 3019 # worsest bid partly cancelled outside price range
+# size = 3019 # pass worsest bid partly cancelled outside price range
+# size = 3225 # pass partly cancel the order oustside the price range
+# size = 3444 
 size = 4000 
+
+data_adjuster = DataAdjuster(df2)
+
+# =============================================================================
+# 04 START ALGORITHM
+# =============================================================================
 
 for index in range(size):
     
@@ -299,7 +124,7 @@ for index in range(size):
             order_book.bids.update_order(message)
             message = None
         elif ttype == 3:
-            # if index == 2900: breakpoint()
+            # if index == 3225: breakpoint()
             if price > best_bid:
                 message = None
             else:
@@ -339,12 +164,12 @@ for index in range(size):
         print("The order book now is:")
         print(order_book)
         
-    if index == 3019: breakpoint()
-    order_book = adjust_data_drift(order_book, timestamp, index)
+    if index == 3444: breakpoint()
+    order_book = data_adjuster.adjust_data_drift(order_book, timestamp, index)
     print("brief_order_book(order_book)")
-    print(brief_order_book(order_book))
+    print(utils.brief_order_book(order_book))
     # order_book.asks = None # remove the ask side
-    assert is_right_answer(order_book, index), "the orderbook if different from the data"
+    assert utils.is_right_answer(order_book, index), "the orderbook if different from the data"
     print("=="*10 + "=" + "=====" + "="+ "=="*10+'\n')
     
    
