187b4b466b932e6972718dd7dfe45b1cff5a81cc
KangOxford <kang.li@maths.ox.ac.uk>
2022-10-26 00:47:37 +0100

    BaseEnv

--- gym_exchange/base_environment/base_env.py
+++ gym_exchange/base_environment/base_env.py
@@ -1,4 +1,62 @@
 # -*- coding: utf-8 -*-
+from gym import Env
+from gym import spaces
+from gym_exchange.base_environment import Configuration
 
 class BaseEnv():
-    pass
+    """A stock trading environment based on OpenAI gym"""
+    metadata = {'render.modes': ['human']}
+    # -------------------------- 01 ----------------------------
+    def __init__(self):
+        super().__init__()
+        self.space_definition()
+        
+    def space_definition(self):
+        self.action_space = spaces.Box(
+            0, Configuration.max_action,shape =(1,),
+            dtype = np.int32)
+        self.observation_space = \
+            spaces.Box(
+                low = np.array([Flag.min_price] * 10 +\
+                            [Flag.num2liquidate]*1 +\
+                            [Flag.max_episode_steps]*1 +\
+                            [Flag.min_quantity]*10 + \
+                            [Flag.min_num_left]*1 +\
+                            [Flag.min_step_left]*1 
+                            ).reshape((Flag.state_dim_1,Flag.state_dim_2)),
+                high = np.array(
+                            [Flag.max_price] * 10 +\
+                            [Flag.num2liquidate]*1 +\
+                            [Flag.max_episode_steps]*1 +\
+                            [Flag.max_quantity]*10 + \
+                            [Flag.max_num_left]*1 +\
+                            [Flag.max_step_left]*1 
+                            ).reshape((Flag.state_dim_1,Flag.state_dim_2)),
+                shape = (Flag.state_dim_1,Flag.state_dim_2),
+                dtype = np.int32,)
+    # -------------------------- 02 ----------------------------
+    def reset(self):
+        pass
+    
+    # -------------------------- 03 ----------------------------
+    def step(self, action):
+        '''input : action
+           return: observation, reward, done, info'''
+        return observation, reward, done, info
+        # ····················· 03.01 ·····················
+        @property
+        def observation(self):
+            pass
+        # ····················· 03.01 ·····················
+        @property
+        def reward(self):
+            pass
+        # ····················· 03.01 ·····················
+        @property
+        def done(self):
+            pass
+        # ····················· 03.01 ·····················
+        @property
+        def info(self):
+            pass
+        
---
+++ gym_exchange/base_environment/metric.py
@@ -0,0 +1,4 @@
+class Vwap():
+    def __init__(self):
+        pass
+    
