954463d5704af044e3809e032923bf8b605e1764
KangOxford <kang.li@maths.ox.ac.uk>
2022-10-26 00:21:33 +0100

    milestone

--- gym_exchange/exchange/exchange.py
+++ gym_exchange/exchange/exchange.py
@@ -28,13 +28,15 @@ class Exchange():
         self.index = 0
         self.encoder, self.flow_list = self.initialization()
         self.reset()
-    def initialize_orderbook(self, order_book):
-        for flow in self.flow_list:
-            order_book.process_order(flow.to_message, True, False)
+    def initialize_orderbook(self):
+        for _ in range(2*Configuration.price_level):
+            flow = next(self.flow_generator)
+            self.order_book.process_order(flow.to_message, True, False)
             self.index += 1
-            if self.index >= 2*Configuration.price_level:break #TODO not sure about the num
+            # if self.index >= 2*Configuration.price_level:break #TODO not sure about the num
         # for index in range():
         #     order_book.process_order(self.flow_list[index])
+        print()#$
             
     def initialization(self):
         # -------------------------- 03.01 ----------------------------
@@ -45,36 +47,42 @@ class Exchange():
         return encoder, flow_list
     def reset(self):
         self.order_book = OrderBook()
-        self.initialize_orderbook(self.order_book)
+        self.flow_generator = self.generate_flow()
+        self.initialize_orderbook()
     def generate_flow(self):
         for flow in self.flow_list:
             yield flow
-    def step(self):
+    def step(self, action = None):
         # -------------------------- 03.02 ----------------------------
-        for flow in self.flow_list:
-            # order_book.process(flow.to_order)
-            message = flow.to_message
-            if flow.type == 1:
-                self.order_book.process_order(message, True, False)
-            elif flow.type == 2:
-                pass #TODO, not implemented!!
-            elif flow.type == 3:
-                pass #TODO, should be partly cancel
-                # order_book.cancel_order(side = message['side'], 
-                #                         order_id = message['order_id'],
-                #                         time = message['timestamp'], 
-                #                         # order_id = order.order_id,
-                #                         # time = order.timestamp, 
-                #                         )
-        if Debugger.on:
-            print(self.order_book)#$
-            print(utils.brief_order_book(self.order_book, side = 'bid'))#$
-            print(utils.brief_order_book(self.order_book, side = 'ask'))#$
+        # for flow in self.flow_list:
+        # order_book.process(flow.to_order)
+        flow = next(self.flow_generator)
+        for item in [flow, action]:
+            if item is not None:
+                message = item.to_message
+                if item.type == 1:
+                    self.order_book.process_order(message, True, False)
+                elif item.type == 2:
+                    pass #TODO, not implemented!!
+                elif item.type == 3:
+                    pass #TODO, should be partly cancel
+                    # order_book.cancel_order(side = message['side'], 
+                    #                         order_id = message['order_id'],
+                    #                         time = message['timestamp'], 
+                    #                         # order_id = order.order_id,
+                    #                         # time = order.timestamp, 
+                    #                         )
+                if Debugger.on:
+                    print(self.order_book)#$
+                    print(utils.brief_order_book(self.order_book, side = 'bid'))#$
+                    print(utils.brief_order_book(self.order_book, side = 'ask'))#$
+        return self.order_book
 
 
 if __name__ == "__main__":
     exchange = Exchange()
-    exchange.step()
+    for _ in range(1000):
+        exchange.step()
     
 
 
