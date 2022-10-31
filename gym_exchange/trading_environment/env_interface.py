783a3e439724ca160158a6a2b1dcec7731a7cbb5
KangOxford <kang.li@maths.ox.ac.uk>
2022-10-31 16:46:29 +0000

    Update env_interface.py

--- gym_exchange/trading_environment/env_interface.py
+++ gym_exchange/trading_environment/env_interface.py
@@ -1,52 +1,57 @@
 # -*- coding: utf-8 -*-
 import abc
+import gym
 import numpy as np
-from gym import Env
 from gym import spaces
-from gym_exchange.metric import Vwap
-from gym_exchange.action import SimpleAction
-from gym_exchange.action import BaseAction
-from gym_exchange.base_environment import Conf # Configuration
+from typing import Generic, Optional, Sequence, Tuple, TypeVar
 
+State = TypeVar("State")
+Observation = TypeVar("Observation")
+Action = TypeVar("Action")
 
-class EnvInterface(abc.ABC):
+class EnvInterface(gym.Env, abc.ABC, Generic[State, Observation, Action]):
     """A stock trading environment based on OpenAI gym"""
     metadata = {'render.modes': ['human']}
-    # -------------------------- 01 ----------------------------
+    # ========================== 01 ==========================
     def __init__(self):
         super().__init__()
         self.space_definition()
         self.vwap_estimator = Vwap()
+        self.seed()
     
     @abc.abstractmethod    
     def space_definition(self):
         pass
-    # -------------------------- 02 ----------------------------
+    # ========================== 02 ==========================
     @abc.abstractmethod
     def reset(self):
         pass
     
-    # -------------------------- 03 ----------------------------
+    # ========================== 03 ==========================
     @abc.abstractmethod
     def step(self, action):
         '''input : action
            return: observation, reward, done, info'''
         return self.observation, self.reward, self.done, self.info
-        # ····················· 03.01 ·····················
+        # --------------------- 03.01 ---------------------
         @property
         def observation(self):
             pass
-        # ····················· 03.02 ·····················
+        # ···················· 03.01.01 ···················· 
+        @abc.abstractmethod
+        def obs_from_state(self, state: State) -> Observation:
+            """Sample observation for given state."""
+        # --------------------- 03.02 ---------------------
         @property
         def reward(self):
             difference = self.vwap_estimator.difference
             return reward
-        # ····················· 03.03 ·····················
+        # --------------------- 03.03  ---------------------
         @property
         def done(self):
             pass
-        # ····················· 03.04 ·····················
+        # --------------------- 03.04 ---------------------  
         @property
         def info(self):
             pass
-        
+ 
