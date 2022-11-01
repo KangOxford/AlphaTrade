960d87a095deed8a11aee28e25b02b6e976bd8a8
KangOxford <kang.li@maths.ox.ac.uk>
2022-11-01 20:12:49 +0000

    bids and asks

--- gym_exchange/trading_environment/base_env.py
+++ gym_exchange/trading_environment/base_env.py
@@ -1,7 +1,7 @@
 import numpy as np
 from gym import Env
 from gym import spaces
-from gym_exchange.exchange import Exchange
+from gym_exchange.exchange.exchange import Exchange
 from gym_exchange.trading_environment import Config
 from gym_exchange.trading_environment.reward import RewardGenerator
 from gym_exchange.trading_environment.action import Action
@@ -44,6 +44,11 @@ class BaseEnv(EnvInterface):
         """Samples from the initial state distribution."""
         self.cur_step = 0
         self.num_left = Config.num2liquidate
+        order_book = self.exchange.order_book #$
+        from gym_exchange.data_orderbook_adapter.utils import brief_order_book
+        asks = brief_order_book(order_book, 'ask')
+        bids = brief_order_book(order_book, 'bid')
+        state = 0
         return state
         
     # ========================== 03 ==========================
