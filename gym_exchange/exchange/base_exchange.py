50d50626c8213eaba45739811e3ae44eadcbefa7
KangOxford <kang.li@maths.ox.ac.uk>
2022-11-04 15:50:29 +0000

    base_exchange

--- gym_exchange/exchange/base_exchange.py
+++ gym_exchange/exchange/base_exchange.py
@@ -15,21 +15,35 @@ from gym_exchange.data_orderbook_adapter import utils
 # from gym_exchange.trading_environment.env_interface import State, Observation, Action # types
 
 # ========================= 03 =========================
-class Exchange(Exchange_Interface):
+class BaseExchange(Exchange_Interface):
     def __init__(self):
         super().__init__()
         
     # -------------------------- 03.01 ----------------------------
     def reset(self):
         super().reset()
-        self.auto_cancels = AutoCancels()
-
-        
+        self.executed_pairs = ExecutedPairs()
+        self.part_deletion_handler = PartDeletionHandler()
+        self.total_deletion_handler = TotalDeletionHandler()
+    
     def update_task_list(self, action = None):# action : Action(for the definition of type)
-        super().update_task_list(action)
-        auto_cancels = self.auto_cancels.step(); auto_cancels = [self.time_wrapper(auto_cancel) for auto_cancel in auto_cancels] # used for auto cancel
-        self.task_list += auto_cancels    
-            
+        flow = next(self.flow_generator)#used for historical data
+        self.task_list = [action, flow] 
+        
+    def process_task(self): # para: self.task_list; return: self.order_book
+        for index, item in enumerate(self.task_list): # advantange for ask limit order (in liquidation problem)
+            if item is not None:
+                message = item.to_message
+                if item.type == 1:
+                    trades, order_in_book = self.order_book.process_order(message, True, False)
+                    self.executed_pairs.step(trades, 'agent' if index == 0 else 'market') # 2nd para: kind
+                elif item.type == 2:
+                    print(f'>>> type2 activated')#$ 
+                    pass #TODO, not implemented!!
+                elif item.type == 3:
+                    print(f'>>> type3 activated')#$ 
+                    pass #TODO, should be partly cancel
+        
     # -------------------------- 03.02 ----------------------------
     def step(self, action = None): # action : Action(for the definition of type)
         self.update_task_list(action)
@@ -42,7 +56,7 @@ class Exchange(Exchange_Interface):
         return timestamp_increase(timestamp, order_flow) 
     
 if __name__ == "__main__":
-    exchange = Exchange()
+    exchange = BaseExchange()
     exchange.reset()
     for _ in range(1000):
         exchange.step()
--- gym_exchange/exchange/exchange_interface.py
+++ gym_exchange/exchange/exchange_interface.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 # ========================= 01 =========================
+from gym_exchange.orderbook import OrderBook
 from gym_exchange.data_orderbook_adapter.decoder import Decoder
 from gym_exchange.data_orderbook_adapter.encoder import Encoder
 from gym_exchange.data_orderbook_adapter.data_pipeline import DataPipeline
+from gym_exchange.data_orderbook_adapter import Configuration
+
 # ========================= 02 =========================
 import abc; from abc import abstractclassmethod
 class Exchange_Interface(abc.ABC):
@@ -23,9 +26,21 @@ class Exchange_Interface(abc.ABC):
             item.side = side
         return flow_list
     
-    @abstractclassmethod
+
     def reset(self):
-        pass
+        self.order_book = OrderBook()
+        self.flow_generator = self.generate_flow()
+        self.initialize_orderbook()
+        
+    def initialize_orderbook(self):
+        for _ in range(2*Configuration.price_level):
+            flow = next(self.flow_generator)
+            self.order_book.process_order(flow.to_message, True, False)
+            self.index += 1
+            
+    def generate_flow(self):
+        for flow in self.flow_list: yield flow
+        
     @abstractclassmethod
     def step(self):
         pass
