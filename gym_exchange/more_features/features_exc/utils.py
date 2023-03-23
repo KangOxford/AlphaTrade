a49be8b74546aea11662a69a2fc36af926e3b29d
KangOxford <kang@robots.ox.ac.uk>
2023-03-24 03:11:19 +0800

    broadcast

--- gym_exchange/more_features/features_exc/utils.py
+++ gym_exchange/more_features/features_exc/utils.py
@@ -3,14 +3,12 @@ from gym_exchange.data_orderbook_adapter.utils import brief_order_book
 from gym_exchange.trading_environment.basic_env.utils import broadcast_lists
 
 
-broadcast = lambda state_memo_tuple:broadcast_lists(*state_memo_tuple)
-
 def get_state_memo(order_book):
     state_memo_tuple = tuple(map(lambda side: brief_order_book(order_book, side), ('ask', 'bid')))
     try:
         assert len(state_memo_tuple[0]) == len(state_memo_tuple[1])
     except:
-        array = broadcast(state_memo_tuple)
+        array =  broadcast_lists(*state_memo_tuple)
         state_memo_tuple = (array[0], array[1])
     sm = np.array([[[list[2 * i], list[2 * i + 1]] for i in range(len(list) // 2)] for list in state_memo_tuple])
     sm[0] = sm[0][np.argsort(sm[0][:, 0])[::-1]]
