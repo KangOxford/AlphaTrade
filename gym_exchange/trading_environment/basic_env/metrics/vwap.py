e1d26fee30cf7927697bfd72826d7fcae92a2a6a
KangOxford <kang@robots.ox.ac.uk>
2023-03-25 04:54:39 +0800

    going to change the stepvwap

--- gym_exchange/trading_environment/basic_env/metrics/vwap.py
+++ gym_exchange/trading_environment/basic_env/metrics/vwap.py
@@ -49,34 +49,34 @@ class Vwap(abc.ABC):
     
 # ========================== 02 ==========================
 
-# class StepVwap(Vwap):
-#     def __init__(self):
-#         super().__init__()
+class StepVwap(Vwap):
+    def __init__(self):
+        super().__init__()
 
-#     def update(self, executed_pairs):
-#         if len(executed_pairs.market_pairs) == 0 or len(executed_pairs.agent_pairs) == 0 :
-#             if len(executed_pairs.market_pairs) == 0: self.market_vwap = 0
-#             if len(executed_pairs.agent_pairs) == 0: self.agent_vwap = 0
-#             print()# % check self.market_vwap
-#         else:
-#             self.market_pairs = executed_pairs.market_pairs[-1]
-#             self.agent_pairs  = executed_pairs.agent_pairs[-1]
+    def update(self, executed_pairs):
+        if len(executed_pairs.market_pairs) == 0 or len(executed_pairs.agent_pairs) == 0 :
+            if len(executed_pairs.market_pairs) == 0: self.market_vwap = 0
+            if len(executed_pairs.agent_pairs) == 0: self.agent_vwap = 0
+            print()# % check self.market_vwap
+        else:
+            self.market_pairs = executed_pairs.market_pairs[-1]
+            self.agent_pairs  = executed_pairs.agent_pairs[-1]
 
-#     @Vwap.market_vwap.setter
-#     def market_vwap(self, value):
-#         self._market_vwap = value
+    @Vwap.market_vwap.setter
+    def market_vwap(self, value):
+        self._market_vwap = value
 
-#     @Vwap.agent_vwap.setter
-#     def agent_vwap(self, value):
-#         self._agent_vwap = value
+    @Vwap.agent_vwap.setter
+    def agent_vwap(self, value):
+        self._agent_vwap = value
 
-#     @property
-#     def info_dict(self):
-#         return {
-#             "StepVwap/MarketVwap"  :self.market_vwap,
-#             "StepVwap/AgentVwap"   :self.agent_vwap,
-#             "StepVwap/VwapSlippage":self.vwap_slippage
-#         }
+    @property
+    def info_dict(self):
+        return {
+            "StepVwap/MarketVwap"  :self.market_vwap,
+            "StepVwap/AgentVwap"   :self.agent_vwap,
+            "StepVwap/VwapSlippage":self.vwap_slippage
+        }
 
 class EpochVwap(Vwap):
     def __init__(self):
@@ -119,7 +119,7 @@ class EpochVwap(Vwap):
 # ========================== 04 ==========================
 class VwapEstimator():
     def __init__(self):
-        # self.step_vwap = StepVwap() # Used for info
+        self.step_vwap = StepVwap() # Used for info
         self.epoch_vwap= EpochVwap()# Used for info
     def executed_pairs_adapter(self, executed_pairs):
         market_pairs_dict = executed_pairs.market_pairs
@@ -137,11 +137,11 @@ class VwapEstimator():
             self.epoch_vwap.update(executed_pairs)
     def step(self):
         if not self.done:
-            return None, None
-            # return self.step_vwap.info_dict, None
+            # return None, None
+            return self.step_vwap.info_dict, None
         else:
-            return None, self.epoch_vwap.info_dict
-            # return self.step_vwap.info_dict, self.epoch_vwap.info_dict
+            # return None, self.epoch_vwap.info_dict
+            return self.step_vwap.info_dict, self.epoch_vwap.info_dict
         
         
 if __name__ == "__main__":
