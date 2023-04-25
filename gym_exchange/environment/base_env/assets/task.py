51b17abd7ae05b7ee64debfd971102a3dcf0c8b4
KangOxford <kang@robots.ox.ac.uk>
2023-04-25 21:34:02 +0100

    cleaned up for NumLeftProcessor

--- gym_exchange/environment/base_env/assets/task.py
+++ gym_exchange/environment/base_env/assets/task.py
@@ -3,7 +3,6 @@ import numpy as np
 from gym_exchange import Config
 def get_slice(aps, real_executed):
     total_quantity = np.sum(aps[1])
-
     if real_executed > total_quantity:
         print("Not enough quantity available.")
     else:
@@ -22,19 +21,28 @@ def get_slice(aps, real_executed):
         print(aps_slice)
         return aps_slice
 
+# def get_slice(aps, real_executed):
+#     total_quantity = np.sum(aps[1])
+#     if real_executed > total_quantity:
+#         print("Not enough quantity available.")
+#     else:
+#         # Find the slice_end index using np.cumsum and np.argmax
+#         cum_qty = np.cumsum(aps[1])
+#         slice_end = np.argmax(cum_qty >= real_executed) + 1
+#
+#         aps_slice = aps[:, :slice_end]
+#         aps_slice[1, -1] = real_executed - cum_qty[slice_end - 2]
+#
+#         return aps_slice
+
 class NumLeftProcessor():
     def __init__(self):
         self.num_left = Config.num2liquidate
         self.agent_executed_pairs_in_last_step = None # used for the first step as there is not forward step
         self.num_executed_in_last_step = 0 # used for the first step as there is not forward step
     def step(self,Self):
-        # self.num_left -= self.cur_action
         agent_executed_pairs_in_last_step = Self.exchange.executed_pairs_recoder.market_agent_executed_pairs_in_last_step['agent_pairs']
-        # print(f"agent_executed_pairs_in_last_step:{agent_executed_pairs_in_last_step}")#$
-        if agent_executed_pairs_in_last_step is None:
-            # print("*** no agent_executed_pairs in the last step")
-            pass
-        else:
+        if agent_executed_pairs_in_last_step is not None:
             last_executed = agent_executed_pairs_in_last_step[1,:].sum()
             real_executed = min(self.num_left, last_executed)
             self.num_left = max(0, self.num_left - last_executed)
@@ -42,13 +50,6 @@ class NumLeftProcessor():
                 aps = Self.exchange.executed_pairs_recoder.market_agent_executed_pairs_in_last_step['agent_pairs']
                 aps_slice = get_slice(aps, real_executed)
                 Self.exchange.executed_pairs_recoder.market_agent_executed_pairs_in_last_step['agent_pairs'] = aps_slice
-                print() #$
-            # self.num_left -= last_executed
-            # if self.num_left < 0:
-            #     real_executed = self.num_left + last_executed
-            #     print() #$
-            # if agent_executed_pairs_in_last_step[1,:].__len__() != 1:
-            #     breakpoint()
         self.num_executed_in_last_step = 0 if agent_executed_pairs_in_last_step is None else real_executed
         assert self.num_left >= 0
 
