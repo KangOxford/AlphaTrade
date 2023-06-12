bb6a85c99a1c39a7a7694cc0b5de6b8d964342cd
Sascha Frey <saschayvesfrey@gmail.com>
2023-06-12 10:50:01 +0100

    Testing

--- gymnax_exchange/test_scripts/testing_base_env.py
+++ gymnax_exchange/test_scripts/testing_base_env.py
@@ -4,6 +4,8 @@ import gymnax
 import sys
 sys.path.append('/Users/sasrey/AlphaTrade')
 from gymnax_exchange.jaxen.base_env import BaseLOBEnv
+from gymnax_exchange.jaxes.jaxob_new import JaxOrderBookArrays as job
+import chex
 
 import faulthandler
 
@@ -20,12 +22,18 @@ env_params=env.default_params
 print(env_params)
 
 obs,state=env.reset(key_reset,env_params)
+print(obs)
+print(state)
+print(state.window_index)
+print(state.step_counter)
+print(env_params.message_data.shape)
 
+print(job.get_data_messages(env_params.message_data,state.window_index,state.step_counter+1))
 
 #print(env.action_space().sample(key_policy))
 #print(env.state_space(env_params).sample(key_policy))
 
-obs,state,reward,done,info=env.step(key_step, state,env.action_space().sample(key_policy), env_params)
+#obs,state,reward,done,info=env.step(key_step, state,env.action_space().sample(key_policy), env_params)
 #print(obs)
 
 """obs,state,reward,done,info=env.step(key_step, state,env.action_space().sample(key_policy), env_params)
