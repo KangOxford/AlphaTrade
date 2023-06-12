82199d204ddcd0e307a1a931c6abdbdbf7b5c631
Sascha Frey <saschayvesfrey@gmail.com>
2023-06-12 10:50:28 +0100

    Fixed symbol=None

--- gym_exchange/__init__.py
+++ gym_exchange/__init__.py
@@ -4,8 +4,6 @@ import numpy as np
 from os import listdir;from os.path import isfile, join
 
 def get_symbol_date(AlphaTradeRoot):
-    symbol=None
-    date=None
     data_path = AlphaTradeRoot+"data"
     onlyfiles = sorted([f for f in listdir(data_path) if isfile(join(data_path, f))])
     for filename in onlyfiles:
