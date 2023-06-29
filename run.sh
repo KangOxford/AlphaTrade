6c5d4c2b08323abd825fa49973c80603aab3e15d
KangOxford <kang@robots.ox.ac.uk>
2023-06-30 00:15:11 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-#cd /homes/80/kang/SpeedTesting/
-#touch OutPut4.1.txt
-#touch OutPut4.2.txt
+
 
 python -m cProfile -s cumulative /homes/80/kang/SpeedTesting/train/sb3/train_v4.1.py > /homes/80/kang/SpeedTesting/OutPut4.1.txt
 python -m cProfile -s cumulative /homes/80/kang/SpeedTesting/train/sb3/train_v4.2.py > /homes/80/kang/SpeedTesting/OutPut4.2.txt
@@ -8,11 +6,7 @@ python -m cProfile -s cumulative /homes/80/kang/SpeedTesting/train/sb3/train_v4.
 
 python -m cProfile -s cumulative /homes/80/kang/SpeedTesting/train/sb3/train_v4.1.1.py > /homes/80/kang/SpeedTesting/OutPut4.1.1.txt
 
-#/homes/80/kang/SpeedTesting/train/sb3/train_v4.1.1.py
 
-#cd /homes/80/kang/SpeedTesting/
-#touch OutPut4.1.1.txt
-#touch OutPut4.1.2.txt
 
 
 python -m cProfile -s cumulative /homes/80/kang/SpeedTesting/train/sb3/train_v4.1.1.py > /homes/80/kang/SpeedTesting/OutPut4.1.1.txt
@@ -21,6 +15,4 @@ python -m cProfile -s cumulative /homes/80/kang/SpeedTesting/train/sb3/train_v4.
 
 
 
-#cd /home/kanli/speedTesting/
-#touch OutPut4.1.2.2.txt
 python -m cProfile -s cumulative /home/kanli/speedTesting/train/sb3/train_v4.1.2.2.py > /home/kanli/speedTesting/OutPut4.1.2.2.txt
