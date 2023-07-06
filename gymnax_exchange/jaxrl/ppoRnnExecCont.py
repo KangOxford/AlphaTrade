f086439f4508b80624a43e404b3a739de81bf4ee
kang <kang@robots.ox.ac.uk>
2023-07-06 06:34:34 +0000

    [done] save and restore trained params

--- gymnax_exchange/jaxrl/ppoRnnExecCont.py
+++ gymnax_exchange/jaxrl/ppoRnnExecCont.py
@@ -429,7 +429,7 @@ if __name__ == "__main__":
         "NUM_STEPS": 10,
         # "TOTAL_TIMESTEPS": 1e7,
         # "TOTAL_TIMESTEPS": 5e5,
-        "TOTAL_TIMESTEPS": 5e4,
+        "TOTAL_TIMESTEPS": 1e4,
         "UPDATE_EPOCHS": 4,
         "NUM_MINIBATCHES": 4,
         "GAMMA": 0.99,
@@ -460,3 +460,34 @@ if __name__ == "__main__":
     print("Time: ", time.time()-start)
     
     # run.finish()
+    
+
+    # '''
+    # # ---------- Save Output ----------
+    import flax
+    from flax.training import checkpoints
+
+    train_state = out['runner_state'][0] # runner_state.train_state
+    params = out['runner_state'][0].params
+
+    # # Save the state to a checkpoint directory
+    # CKPT_DIR = '/homes/80/kang/AlphaTrade/checkpoints/'
+    # checkpoints.save_checkpoint(ckpt_dir=CKPT_DIR, target=train_state, step=0)
+    
+    # # Create a state object
+    # state = train_state.TrainState.create(apply_fn=model.apply, params=params, ...)
+    # # # Restore the state from the checkpoint directory
+    # restored_state = checkpoints.restore_checkpoint(ckpt_dir=CKPT_DIR, target=state)
+    
+    # Save the params to a file using flax.serialization.to_bytes
+    with open('params_file', 'wb') as f:
+        f.write(flax.serialization.to_bytes(params))
+
+    # Load the params from the file using flax.serialization.from_bytes
+    with open('params_file', 'rb') as f:
+        params = flax.serialization.from_bytes(params, f.read())
+  
+    jax.debug.breakpoint()
+    print(">>>")
+    # '''
+    
