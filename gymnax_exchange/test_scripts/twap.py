58069e7c05ffd28382abe1c17bd52a6bf8acd24a
kang <kang@robots.ox.ac.uk>
2023-07-15 11:08:46 +0000

    adding round() to action from pi round => ceil in twap random shuffle [stepQuant - 3*stepQuant//4,stepQuant//4, stepQuant//4, stepQuant//4]

--- gymnax_exchange/test_scripts/twap.py
+++ gymnax_exchange/test_scripts/twap.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
         ATFolder = sys.argv[1]
         print("AlphaTrade folder:",ATFolder)
     except:
-        ATFolder = '/homes/80/kang/AlphaTrade'
+        ATFolder = "/homes/80/kang/AlphaTrade/testing_small"
+        # ATFolder = '/homes/80/kang/AlphaTrade'
         
     rng = jax.random.PRNGKey(0)
     rng, key_reset, key_policy, key_step = jax.random.split(rng, 4)
@@ -39,9 +40,12 @@ if __name__ == "__main__":
     print(env_params.message_data.shape, env_params.book_data.shape)
 
     i = 1
-    for i in range(1,100):
+    for i in range(1,10000):
         # ==================== ACTION ====================
         # ---------- acion from given strategy  ----------
+        print("---"*20)
+        print("window_index ",state.window_index)
+        key_policy, _ = jax.random.split(key_policy,2)
         test_action=env.action_space().sample(key_policy)
         
         # env_state, last_obs, last_done, rng = runner_state
@@ -53,64 +57,49 @@ if __name__ == "__main__":
         # obsv_step, env_state_step, reward_step, done_step, info_step = jax.vmap(
         #     env.step, in_axes=(0, 0, 0, None)
         # )(rng_step, env_state, action, env_params)
-        
-        def twap(obs, state, params):
+        # if i == 277:
+        #     jax.debug.breakpoint()
+        def twap(state, env_params):
             # ---------- ifMarketOrder ----------
-            remainingTime = params.episode_time - jnp.array((state.time-state.init_time)[0], dtype=jnp.int32)
+            remainingTime = env_params.episode_time - jnp.array((state.time-state.init_time)[0], dtype=jnp.int32)
             marketOrderTime = jnp.array(60, dtype=jnp.int32) # in seconds, means the last minute was left for market order
             ifMarketOrder = (remainingTime <= marketOrderTime)
+            print(f"{i} remainingTime{remainingTime} marketOrderTime{marketOrderTime}")
             # ---------- ifMarketOrder ----------
             # ---------- quants ----------
             remainedQuant = state.task_to_execute - state.quant_executed
-            remainedStep = params.max_steps_in_episode - state.step_counter
-            stepQuant = jnp.where(ifMarketOrder,remainedQuant,remainedQuant//remainedStep)
-            quants = [stepQuant//4, stepQuant//4, stepQuant//4, stepQuant - 3*stepQuant//4]
+            remainedStep = state.max_steps_in_episode - state.step_counter
+            stepQuant = jnp.ceil(remainedQuant/remainedStep).astype(jnp.int32) # for limit orders
+            # quants = [stepQuant - 3*stepQuant//4,stepQuant//4, stepQuant//4, stepQuant//4]
+            # quants = jax.random.permutation(key_policy, jnp.array([stepQuant - 3*stepQuant//4,stepQuant//4, stepQuant//4, stepQuant//4]), independent=True)
+            # limit_quants_agressive = jax.random.permutation(key_policy, jnp.array([stepQuant - 3*stepQuant//4,stepQuant//4, stepQuant//4,stepQuant//4]), independent=True)
+            # limit_quants_agressive = jax.random.permutation(key_policy, jnp.array([stepQuant - 3*stepQuant//4,stepQuant//4, stepQuant//4, stepQuant//4]), independent=True).at[-1].set(max(stepQuant - 3*stepQuant//4,stepQuant//4))
+            limit_quants_agressive = jnp.append(jax.random.permutation(key_policy, jnp.array([stepQuant - 3*stepQuant//4,stepQuant//4, stepQuant//4]), independent=True),max(stepQuant - 3*stepQuant//4,stepQuant//4))
+            # limit_quants_passive = jax.random.permutation(key_policy, jnp.array([remainedQuant - 3*remainedQuant//4,remainedQuant//4, remainedQuant//4,remainedQuant//4]), independent=True)
+            limit_quants_passive = jnp.append(jax.random.permutation(key_policy, jnp.array([remainedQuant - 3*remainedQuant//4,remainedQuant//4, remainedQuant//4]), independent=True),remainedQuant//4)
+            limit_quants = jnp.where(limit_quants_agressive.sum() <= remainedQuant,limit_quants_agressive,limit_quants_passive)
+            # assert limit_quants.sum() <= remainedQuant, f"{limit_quants}, {limit_quants.sum()} should less_equal than {remainedQuant}"
+            # if not limit_quants_agressive.sum() <= remainedQuant: print("+++")
+            market_quants = jnp.array([remainedQuant - 3*remainedQuant//4,remainedQuant//4, remainedQuant//4, remainedQuant//4])
+            quants = jnp.where(ifMarketOrder,market_quants,limit_quants)
             # ---------- quants ----------
             return jnp.array(quants)
             
-            
-        action_twap = twap(obs, state, env_params)
+        twap_action = twap(state, env_params)
         
         
-        
-        # ---------- acion from random sampling ----------
-        test_action=env.action_space().sample(key_policy)
-        # ---------- acion from trained network ----------
-        ac_in = (obs[np.newaxis, :], obs[np.newaxis, :])
-        ## import ** network
-        from gymnax_exchange.jaxrl.ppoRnnExecCont import ActorCriticRNN
-        ppo_config = {
-            "LR": 2.5e-4,
-            "NUM_ENVS": 4,
-            "NUM_STEPS": 2,
-            "TOTAL_TIMESTEPS": 5e5,
-            "UPDATE_EPOCHS": 4,
-            "NUM_MINIBATCHES": 4,
-            "GAMMA": 0.99,
-            "GAE_LAMBDA": 0.95,
-            "CLIP_EPS": 0.2,
-            "ENT_COEF": 0.01,
-            "VF_COEF": 0.5,
-            "MAX_GRAD_NORM": 0.5,
-            "ENV_NAME": "alphatradeExec-v0",
-            "ANNEAL_LR": True,
-            "DEBUG": True,
-            "NORMALIZE_ENV": False,
-            "ATFOLDER": ATFolder,
-            "TASKSIDE":'buy'
-        }
-        # runner_state = np.load("runner_state.npy") # FIXME/TODO save the runner_state after training
-        # network = ActorCriticRNN(env.action_space(env_params).shape[0], config=ppo_config)
-        # hstate, pi, value = network.apply(runner_state.train_state.params, hstate, ac_in)
-        # action = pi.sample(seed=rng) # 4*1, should be (4*4: 4actions * 4envs)
-        # ==================== ACTION ====================
-        
-        
-        print(f"Sampled {i}th actions are: ",test_action)
+        # print(f"Sampled {i}th actions are: ",test_action)
+        print(f"Sampled {i}th actions are: ",twap_action)
         start=time.time()
-        obs,state,reward,done,info=env.step(key_step, state,test_action, env_params)
+        # obs,state,reward,done,info=env.step(key_step, state,test_action, env_params)
+        obs,state,reward,done,info=env.step(key_step, state,twap_action, env_params)
         print(f"State after {i} step: \n",state,done,file=open('output.txt','a'))
         print(f"Time for {i} step: \n",time.time()-start)
+        print("excuted ",info["quant_executed"])
+        if done:
+            break
+    print(info["total_revenue"])
+    print(info["total_revenue"]/info["task_to_execute"])
 
     # ####### Testing the vmap abilities ########
     
