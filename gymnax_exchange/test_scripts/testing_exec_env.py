81ab0ef037576f2011aada9f70a06d67a8dde332
saschafrey <saschayvesfrey@gmail.com>
2023-07-22 08:43:14 +0100

    n_envs varying

--- gymnax_exchange/test_scripts/testing_exec_env.py
+++ gymnax_exchange/test_scripts/testing_exec_env.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         vmap_step = jax.jit(jax.vmap(env.step, in_axes=(0, 0, 0, None)))
         vmap_act_sample=jax.jit(jax.vmap(env.action_space().sample, in_axes=(0)))
 
-        num_envs = 1000
+        num_envs = 10000
         vmap_keys = jax.random.split(rng, num_envs)
 
         test_actions=vmap_act_sample(vmap_keys)
