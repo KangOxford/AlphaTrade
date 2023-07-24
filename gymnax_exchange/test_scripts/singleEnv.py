28a8eaf6a45b7ef95ed503c954f12d13b2b41646
kang <kang@robots.ox.ac.uk>
2023-07-24 13:15:42 +0000

    update

--- gymnax_exchange/test_scripts/singleEnv.py
+++ gymnax_exchange/test_scripts/singleEnv.py
@@ -33,8 +33,12 @@ if __name__ == "__main__":
     assert env.task_size == 500    
     
     # -----------------------------------        
+    import time
+    timestamp = str(int(time.time()))
+    rngInitNum = 1    
     rng = jax.random.PRNGKey(rngInitNum)
-    rng, key_reset, key_policy, key_step = jax.random.split(rng, 4)
+    
+    rng, key_reset, key_policy, key_step, _rng = jax.random.split(rng, 5)
     obs,state=env.reset(key_reset,env_params)
     ppo_config = {
             "LR": 2.5e-4,
@@ -64,10 +68,21 @@ if __name__ == "__main__":
     #     restored_params = flax.serialization.from_bytes(flax.core.frozen_dict.FrozenDict, f.read())
     #     print(f"pramas restored")
         
-    initail_random_params = ???? # TODO
+    # initail_random_params = ???? # TODO
     
     network = ActorCriticRNN(env.action_space(env_params).shape[0], config=ppo_config)
     init_hstate = ScannedRNN.initialize_carry(ppo_config["NUM_ENVS"], 128)
+    init_x = (
+            jnp.zeros(
+                (1, ppo_config["NUM_ENVS"], *env.observation_space(env_params).shape)
+            ),
+            jnp.zeros((1, ppo_config["NUM_ENVS"])),
+        )
+    _rng,_= jax.random.split(2)
+    network_params = network.init(_rng, init_hstate, init_x)
+    
+    
+    initail_random_params=network_params
     init_done = jnp.array([False]*ppo_config["NUM_ENVS"])
     ac_in = (obs[np.newaxis, np.newaxis, :], init_done[np.newaxis, :])
     assert len(ac_in[0].shape) == 3
@@ -77,12 +92,6 @@ if __name__ == "__main__":
     print(f"-------------\nPPO 0th actions are: {action} with sum {action.sum()}")
     obs,state,reward,done,info=env.step(key_step, state, action, env_params)
     print("{" + ", ".join([f"'{k}': {v}" for k, v in info.items()]) + "}")
-    
-    import time
-    timestamp = str(int(time.time()))
-    rngInitNum = 0    
-    rng = jax.random.PRNGKey(rngInitNum)
-    rng, key_reset, key_policy, key_step = jax.random.split(rng, 4)
     start=time.time()
     obs,state=env.reset(key_reset,env_params)
     print("Time for reset: \n",time.time()-start)
@@ -91,18 +100,28 @@ if __name__ == "__main__":
     excuted_list = []
     er = 0
     for i in range(1,10000):
+        network = ActorCriticRNN(env.action_space(env_params).shape[0], config=ppo_config)
+        init_hstate = ScannedRNN.initialize_carry(ppo_config["NUM_ENVS"], 128)
+        init_x = (
+                jnp.zeros(
+                    (1, ppo_config["NUM_ENVS"], *env.observation_space(env_params).shape)
+                ),
+                jnp.zeros((1, ppo_config["NUM_ENVS"])),
+            )
+        _rng,_= jax.random.split(2)
+        network_params = network.init(_rng, init_hstate, init_x)
         # ==================== ACTION ====================
         # ---------- acion from trained network ----------
         ac_in = (obs[np.newaxis,np.newaxis, :], jnp.array([done])[np.newaxis, :])
         assert len(ac_in[0].shape) == 3, f"{ac_in[0].shape}"
         assert len(ac_in[1].shape) == 2, f"{ac_in[1].shape}"
-        hstate, pi, value = network.apply(restored_params, hstate, ac_in) 
+        hstate, pi, value = network.apply(network_params, hstate, ac_in) 
         action = pi.sample(seed=rng).round().astype(jnp.int32)[0,0,:].clip( 0, None)
         # ---------- acion from trained network ----------
         # ==================== ACTION ====================    
-        print(f"Sampled {i}th actions are: ",twap_action)
+        print(f"Sampled {i}th actions are: ",action)
         start=time.time()
-        obs,state,reward,done,info=env.step(key_step, state,twap_action, env_params)
+        obs,state,reward,done,info=env.step(key_step, state,action, env_params)
         er+=reward
         print(f"Time for {i} step: \n",time.time()-start)
         print("excuted ",info["quant_executed"])
