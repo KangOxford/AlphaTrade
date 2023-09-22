8123dab694fd3db66fd2010a49dfa0fe27b531be
kang <kang@robots.ox.ac.uk>
2023-09-22 10:56:33 +0000

    u

--- gymnax_exchange/jaxrl/ppoS5ExecCont.py
+++ gymnax_exchange/jaxrl/ppoS5ExecCont.py
@@ -463,8 +463,8 @@ if __name__ == "__main__":
     ppo_config = {
         "LR": 2.5e-4,
         "ENT_COEF": 0.1,
-        "NUM_ENVS": 1000,
-        "TOTAL_TIMESTEPS": 2e7,
+        "NUM_ENVS": 2000,
+        "TOTAL_TIMESTEPS": 1e8,
         
         # "LR": 2.5e-6,
         # "NUM_ENVS": 1,
