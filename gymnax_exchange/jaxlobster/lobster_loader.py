25de53bce65e720e56dfeea269047bce544365a9
saschafrey <saschayvesfrey@gmail.com>
2023-11-15 16:33:24 +0000

    loading class complete, bug with 12 windows

--- gymnax_exchange/jaxlobster/lobster_loader.py
+++ gymnax_exchange/jaxlobster/lobster_loader.py
@@ -3,9 +3,10 @@ from os import listdir
 from os.path import isfile, join
 import warnings
 
+import itertools
 import pandas as pd
 from pandas.errors import SettingWithCopyWarning
-
+import numpy as np
 
 from jax import numpy as jnp
 import jax
@@ -20,19 +21,67 @@ from jax import lax
 class LoadLOBSTER():
     """Docstring
     """
-    def __init__(self,alphatradepath,n_Levels):
+    def __init__(self,
+                 alphatradepath,
+                 n_Levels=10,
+                 type_="fixed_time",
+                 window_length=1800,
+                 n_msg_per_step=100):
         self.atpath=alphatradepath
         self.messagePath = alphatradepath+"/data/Flow_"+str(n_Levels)+"/"
         self.orderbookPath = alphatradepath+"/data/Book_"+str(n_Levels)+"/"
+        self.window_type=type_
+        self.window_length=window_length
+        self.n_messages=n_msg_per_step
+        self.day_start=34200
+        self.day_end=57600
 
     def run_loading(self):
-        messages, orderbooks = self.load_files()
-        pairs = [self.pre_process_msg_ob(msg,ob) for msg,ob in zip(messages,orderbooks)]
-        messages, orderbooks = zip(*pairs)
+        message_days, orderbook_days = self.load_files()
+        pairs = [self.pre_process_msg_ob(msg,ob) 
+                 for msg,ob 
+                 in zip(message_days,orderbook_days)]
+        message_days, orderbook_days = zip(*pairs)
+        slicedCubes_withOB_list = [self.slice_day_no_overlap(msg_day,ob_day) 
+                                   for msg_day,ob_day 
+                                   in zip(message_days,orderbook_days)]
+        cubes_withOB = list(itertools.chain \
+                            .from_iterable(slicedCubes_withOB_list))
+        max_steps_in_windows_arr = jnp.array([m.shape[0] 
+                                              for m,o 
+                                              in cubes_withOB],jnp.int32)
+        cubes_withOB=self.pad_window_cubes(cubes_withOB)
+        loaded_msg_windows,loaded_book_windows=map(jnp.array,
+                                                    zip(*cubes_withOB))
+        n_windows=len(loaded_book_windows)
+        return (loaded_msg_windows,
+                loaded_book_windows,
+                max_steps_in_windows_arr,
+                n_windows)
 
+    def pad_window_cubes(self,cubes_withOB):
+        #Get length of longest window
+        max_win = max(w.shape[0] for w, o in cubes_withOB)
+        new_cubes_withOB = []
+        for cube, OB in cubes_withOB:
+            cube = self.pad_cube(cube, max_win)
+            new_cubes_withOB.append((cube, OB))
+        return new_cubes_withOB
+    
+    def pad_cube(self, cube, target_shape):
+                # Calculate the amount of padding required
+                padding = [(0, target_shape - cube.shape[0]), (0, 0), (0, 0)]
+                padded_cube = np.pad(cube, padding, mode='constant', constant_values=0)
+                return padded_cube
+
+    def slice_day_no_overlap(self, message_day, orderbook_day):
+        sliced_parts, init_OBs = self.split_day_to_windows(message_day, orderbook_day)
+        slicedCubes = [self._slice_to_cube(slice_) for slice_ in sliced_parts]
+        slicedCubes_withOB = zip(slicedCubes, init_OBs)
+        return slicedCubes_withOB
 
     def load_files(self):
-        """Loads the csvs as pandas arrays
+        """Loads the csvs as pandas arrays. Files are seperated by days
 
         Could potentially be optimised to work around pandas, very slow.         
         """
@@ -67,58 +116,114 @@ class LoadLOBSTER():
         message_day['trader_id'] = message_day['order_id']
         orderbook_day.iloc[valid_index,:].reset_index(inplace=True, drop=True)
         return message_day,orderbook_day
-
-    def slice_day_no_overlap(self, message_day, orderbook_day):
-        def index_of_sliceWithoutOverlap_by_lines(start_time, end_time, interval):
-            indices = list(range(start_time, end_time, interval))
-            return indices
-        indices = index_of_sliceWithoutOverlap_by_lines(0, message_day.shape[0]//100*100, 100*100)
-        # def index_of_sliceWithoutOverlap_by_time(start_time, end_time, interval):
-        #     indices = list(range(start_time, end_time, interval))
-        #     return indices
-        # indices = index_of_sliceWithoutOverlap_by_time(start_time, end_time, sliceTimeWindow)
-        
-        def splitMessage(message_day, orderbook_day):
-            sliced_parts = []
-            init_OBs = []
-            for i in range(len(indices) - 1):
-                start_index = indices[i]
-                end_index = indices[i + 1]
-                sliced_part = message_day[(message_day.index > start_index) & (message_day.index <= end_index)]
-                sliced_parts.append(sliced_part)
-                init_OBs.append(orderbook_day.iloc[start_index,:])
-            # # Last sliced part from last index to end_time
-            # start_index = indices[i]
-            # end_index = indices[i + 1]
-            # index_s, index_e = message_day[(message_day.index >= start_index) & (message_day.index < end_index)].index[[0, -1]].tolist()
-            # # index_s, index_e = message_day[(message_day['time'] >= start_index) & (message_day['time'] < end_index)].index[[0, -1]].tolist()
-            # index_s = (index_s // stepLines - 10) * stepLines + index_e % stepLines
-            # assert (index_e - index_s) % stepLines == 0, 'wrong code 32'
-            # last_sliced_part = message_day.loc[np.arange(index_s, index_e)]
-            # sliced_parts.append(last_sliced_part)
-            # init_OBs.append(orderbook_day.iloc[index_s, :])
-            
-            for part in sliced_parts:
-                # print("start")
-                # assert part.time_s.iloc[-1] - part.time_s.iloc[0] >= sliceTimeWindow, f'wrong code 33, {part.time_s.iloc[-1] - part.time_s.iloc[0]}, {sliceTimeWindow}'
-                assert len(sliced_parts) == len(indices)-1, 'wrong code 33'
-                assert part.shape[0] % stepLines == 0, 'wrong code 34'
-            return sliced_parts, init_OBs
-        sliced_parts, init_OBs = splitMessage(message_day, orderbook_day)
-        def sliced2cude(sliced):
-            columns = ['type','direction','qty','price','trader_id','order_id','time_s','time_ns']
-            cube = sliced[columns].to_numpy()
-            cube = cube.reshape((-1, stepLines, 8))
-            return cube
-        # def initialOrderbook():
-        slicedCubes = [sliced2cude(sliced) for sliced in sliced_parts]
-        # Cube: dynamic_horizon * stepLines * 8
-        slicedCubes_withOB = zip(slicedCubes, init_OBs)
-        return slicedCubes_withOB
     
-    def daily_slice_indeces(self,type,overlap,start_time, end_time, interval):
-        if type=="Time":
-            indices = list(range(start_time, end_time, interval))
-        elif type=="Line":
-            indices = list(range(start_time, end_time, interval))
+    def daily_slice_indeces(self,type,start, end, interval, msgs_length=100000):
+        """Returns a list of times of indices at which to cut the daily
+        message data into data windows.
+            Parameters:
+                type (str): "fixed_steps" or "fixed_time" mode
+                start (int): start time of the day or index of
+                                first message to consider
+                end (int): end time or last index to consider.
+                            If = -1 and using fixe steps then end
+                            index is the full array.
+                interval (int): length of an episode window in
+                                terms of time (s) or number of 
+                                steps.
+                msgs_length (int): Only used in the event of 
+                                    end_time=-1. Total number
+                                    of messages in data. 
+            Returns:
+                    indices (List): Either times or indices at which
+                    to slice the data array. 
+        """
+        if type == "fixed_steps":
+            end_index = ((msgs_length-start)
+                            //self.n_messages*self.n_messages+start
+                          if end==-1 else (end-start)
+                            //self.n_messages*self.n_messages+start)
+            indices = list(range(start, end_index, self.n_messages*interval))
+        elif type == "fixed_time":
+            if end==-1:
+                raise IndexError('Cannot use -1 as an index if fixed_time')
+            indices = list(range(start, end, interval))
+            print(indices)
+        else: raise NotImplementedError('Use either "fixed_time" or' 
+                                        + ' "fixed_steps"')
+        if len(indices)<2:
+            raise ValueError("Not enough range to get a slice")
+        return indices
+
+    def split_day_to_windows(self,message,orderbook):
+        """Splits a day of messages into given windows.
+        The windows are either defined by a fixed time interval or a 
+        fixed number of steps, whereby each step is a fixed number of 
+        messages.
+        For each window, the initial orderbook state is taken from the
+        orderbook data. 
+            Parameters:
+                message (Array): Array of all messages in a day
+                orderbook (Array): All order book states in a day.
+                                    1st dim of equal size as message. 
+           Returns:
+                sliced_parts (List): List of arrays each repr. a window.  
+                init_OBs (List): List of arrays repr. init. orderbook
+                                    data for each window. 
+        """
+        indices=self.daily_slice_indeces(self.window_type,
+                                         self.day_start,
+                                         self.day_end,
+                                         self.window_length,
+                                         message.shape[0])
+        sliced_parts = []
+        init_OBs = []
+        for i in range(len(indices) - 1):
+            start_index = indices[i]
+            end_index = indices[i + 1]
+            if self.window_type == "fixed_steps":
+                sliced_part = message[(message.index > start_index) &
+                                             (message.index <= end_index)]
+            elif self.window_type == "fixed_time":
+                index_s, index_e = message[(message['time'] >= start_index) &
+                                            (message['time'] < end_index)].index[[0, -1]].tolist()
+                
+                index_e = ((index_e // self.n_messages + 10) * self.n_messages
+                            + index_s % self.n_messages)
+                assert ((index_e - index_s) 
+                        % self.n_messages == 0), 'wrong code 31'
+                sliced_part = message.loc[np.arange(index_s, index_e)]
+            sliced_parts.append(sliced_part)
+            init_OBs.append(orderbook.iloc[start_index,:])
+        
+            if self.window_type == "fixed_steps":
+                assert len(sliced_parts) == len(indices)-1, 'wrong code 33'
+                for part in sliced_parts:
+                    assert part.shape[0] % self.n_messages == 0, 'wrong code 34'
+            elif self.window_type == "fixed_time":
+                for part in sliced_parts:
+                    assert (part.time_s.iloc[-1] 
+                            - part.time_s.iloc[0] 
+                            >= self.window_length), \
+                            f"wrong code 33, {part.time_s.iloc[-1] - part.time_s.iloc[0]}, {self.window_length}"
+                    assert part.shape[0] % self.n_messages == 0, 'wrong code 34'
+        return sliced_parts, init_OBs
+    
+    def _slice_to_cube(self,sliced):
+        """Turn a 2D pandas table of messages into a 3D numpy array
+        whereby the additional dimension is due to the message
+        stream being split into fixed-size blocks"""
+        columns = ['type','direction','qty','price',
+                   'trader_id','order_id','time_s','time_ns']
+        cube = sliced[columns].to_numpy()
+        cube = cube.reshape((-1, self.n_messages, 8))
+        return cube
+
+
+if __name__ == "__main__":
+    loader=LoadLOBSTER(".",10)
+    print(loader.daily_slice_indeces(loader.window_type,loader.day_start,loader.day_end,loader.window_length))
+    msgs,books,window_lengths,n_windows=loader.run_loading()
+    print(msgs.shape,books.shape,window_lengths,n_windows)
+    print(list(range(0,10+1,2)))
+
 
