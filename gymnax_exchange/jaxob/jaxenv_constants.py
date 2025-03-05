56ecf081900dfbdd961b8256cdfae494387afe61
reuben <reuben@robots.ox.ac.uk>
2025-03-05 15:54:52 +0000

    fix inv pen sign, stop prints

--- gymnax_exchange/jaxob/jaxenv_constants.py
+++ gymnax_exchange/jaxob/jaxenv_constants.py
@@ -1,8 +1,8 @@
 action_space= "fixed_quants"
 reward_space = "zero_inv"
-observation_space = "messages"
+observation_space = "engineered"
 end_fn = "do_nothing"
 n_ticks_in_book =1
 n_actions=8
 reference_price_portfolio_value="best_bid_ask"
-inv_penalty="none"
+inv_penalty="linear"
