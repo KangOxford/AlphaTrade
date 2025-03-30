fix plots, remove tick size normalization again

gymnax_exchange/jaxen/mm_env.py
@@ -979,7 +979,7 @@ class MarketMakingEnv(BaseLOBEnv):
         mid_price = (best_ask + best_bid) // 2
 
         #Select aaggresion parameter
-        gamma_values = jnp.array([0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0], dtype=jnp.float32)  # Risk aversion
+        gamma_values = jnp.array([0.1, 0.2, 0.5, 1, 2, 5, 10, 20], dtype=jnp.float32)  # Risk aversion
         gamma = gamma_values[action]
 
         #Estimate K paramter from data
@@ -996,7 +996,7 @@ class MarketMakingEnv(BaseLOBEnv):
         normalized_time = time_left / params.episode_time
 
         #Reservation price
-        res_price = (mid_price - ((state.inventory)//self.tick_size) * gamma * (vol) * normalized_time)
+        res_price = (mid_price - ((state.inventory)) * gamma * (vol) * normalized_time)
 
         #Spread
         spread = (gamma*vol*normalized_time + (2/gamma) * jnp.log(1 + gamma/k))*self.tick_size
@@ -1590,7 +1590,7 @@ class MarketMakingEnv(BaseLOBEnv):
 
         # Keep track of overall cash balance (same as overall PnL)
         new_cash_balance = state.cash_balance + PnL
-        inventoryValue=new_inventory*(reference_price//self.tick_size)
+        inventoryValue=new_inventory*(reference_price)
         netWorth=new_cash_balance+inventoryValue  
 
         #calculate a fraction of total market activity attributable to us.

gymnax_exchange/test_scripts/mm_env_test.py
@@ -107,6 +107,7 @@ def generate_plots(
     print(f"Data saved to {reward_file}")
     print(f"Last valid step {valid_steps}")
     print(f"Last NetWorth: {netWorth[-1]}")
+    print(f"Last PnL: {total_PnL[-1]}")
 
     # ============================
     # Plotting each reward type separately
@@ -145,6 +146,13 @@ def generate_plots(
     fig, axes = plt.subplots(3, 3, figsize=(15, 15))  # Adjust the grid as needed
 
     # Plot each metric on a separate subplot
+    axes[0, 0].plot(range(plot_until_step), rewards, label="Reward", color='green')
+    axes[0, 0].set_xlabel("Steps")
+    axes[0, 0].set_ylabel("Reward")
+    axes[0, 0].set_title("Reward Over Steps")
+    axes[0, 0].legend()
+
+
     axes[0, 1].plot(range(plot_until_step), inventory, label="Inventory", color='green')
     axes[0, 1].set_xlabel("Steps")
     axes[0, 1].set_ylabel("Inventory")
@@ -264,7 +272,6 @@ if __name__ == "__main__":
     reward_spooner_scaled = np.zeros((test_steps, 1), dtype=int)
     reward_spooner_damped = np.zeros((test_steps, 1), dtype=int)
     reward_delta_netWorth = np.zeros((test_steps, 1), dtype=int)
-    msgs=np.zeros((test_steps,8),dtype=int)
     inventory = np.zeros((test_steps, 1), dtype=int)
     total_PnL = np.zeros((test_steps, 1), dtype=int)
     buyQuant = np.zeros((test_steps, 1), dtype=int)
@@ -292,8 +299,7 @@ if __name__ == "__main__":
         key_policy, _ = jax.random.split(key_policy, 2)
         key_step, _ = jax.random.split(key_step, 2)
         test_action = env.action_space().sample(key_policy) 
-        #jax.debug.print("action{}",test_action)
-        #test_action=7
+        #test_action=8
         
         start = time.time()
         obs, state, reward, done, info = env.step(key_step, state, test_action, env_params)
@@ -328,7 +334,7 @@ if __name__ == "__main__":
     generate_plots(
      rewards,
      reward_portfolio_value,
-    reward_complex,
+     reward_complex,
      reward_spooner,
      reward_spooner_damped,
      reward_spooner_scaled,