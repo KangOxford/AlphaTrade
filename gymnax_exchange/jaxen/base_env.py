0318e6baae7197500c1786cecd7c1529bb5687c4
Valentin <valentinm@hotmail.de>
2025-05-19 12:14:45 +0100

    remove trader id from base env

--- gymnax_exchange/jaxen/base_env.py
+++ gymnax_exchange/jaxen/base_env.py
@@ -159,7 +159,7 @@ class BaseLOBEnv(environment.Environment):
     info(additional=""):
         Prints the person's name and age.
     """
-    def __init__(self,cfg:Configuration, key, alphatradePath, window_selector, sliceTimeWindow, trader_unique_id= -9999999, ep_type="fixed_time"):
+    def __init__(self,cfg:Configuration, key, alphatradePath, window_selector, sliceTimeWindow, ep_type="fixed_time"):
         super().__init__()
         self.window_selector = window_selector
         self.ep_type = ep_type # fixed_steps, fixed_time
@@ -173,7 +173,6 @@ class BaseLOBEnv(environment.Environment):
         self.n_actions=4
         self.n_ticks_in_book = 10 # Depth of PP actions
         self.customIDCounter=0
-        self.trader_unique_id= trader_unique_id #need to be negative now??
         self.tick_size=100
         self.start_resolution = cfg.start_resolution  # Use value from config
         self.cfg = cfg
