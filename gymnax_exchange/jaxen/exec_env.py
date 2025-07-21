3d6675d86e57babcba329f3853a56c5100b637ba
Valentin <valentinm@hotmail.de>
2025-07-21 10:27:58 +0100

    no scaling in exec reward

--- gymnax_exchange/jaxen/exec_env.py
+++ gymnax_exchange/jaxen/exec_env.py
@@ -1649,6 +1649,7 @@ class ExecutionAgent():
             # print("quant shape: {}", quant.shape)
             # print("time shape: {}", time.shape)
             # print("trader id shape: {}", agent_params.trader_id.shape)
+            #jax.debug.print("quant_left: {}", jnp.abs(quant_left))
             
             
             mid_trade = job.create_trade(
@@ -1735,6 +1736,7 @@ class ExecutionAgent():
 
         #jax.debug.print("advantage: {}", advantage)
         #jax.debug.print("vwap: {}", vwap)
+       # jax.debug.print("exec quant left: {}", quant_left)
 
 
         drift = direction_switch * agentQuant * (vwap - agent_state.init_price//self.world_config.tick_size)
@@ -1754,6 +1756,7 @@ class ExecutionAgent():
             revenue - (agent_state.init_price // self.world_config.tick_size) * agentQuant
         )
         
+        #jax.debug.print("reward exec: {}", reward)
 
         # Add other extras
 
@@ -1776,7 +1779,7 @@ class ExecutionAgent():
         "quant_left": quant_left,
         "trade_duration": trade_duration,
         }
-        reward_scaled = reward / 10000
+        reward_scaled = reward
 
 
         if self.cfg.reward_space == "finish_fast":
