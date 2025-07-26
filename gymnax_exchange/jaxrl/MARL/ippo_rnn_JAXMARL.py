4303bc9f0f92f87eeda638f08fcb953715eacaed
saschafrey <saschayvesfrey@gmail.com>
2025-07-26 15:57:34 +0100

    Checkpointing added to main ppo loop. Additional config for eval time period -> for eval during training on holdout dataset. Iterating over train states not networks as don't need the latter and they make checkpointing tricky.

--- gymnax_exchange/jaxrl/MARL/ippo_rnn_JAXMARL.py
+++ gymnax_exchange/jaxrl/MARL/ippo_rnn_JAXMARL.py
@@ -6,6 +6,7 @@ import os
 
 import pandas as pd
 import csv
+import wandb.sdk
 
 from docs.source import conf
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.8"
@@ -25,7 +26,9 @@ import optax
 from flax.linen.initializers import constant, orthogonal # type: ignore
 from typing import Sequence, NamedTuple, Any, Dict
 from flax.training.train_state import TrainState
+from flax.training import orbax_utils
 import distrax
+import orbax.checkpoint as oxcp
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import gc
@@ -141,7 +144,7 @@ def make_train(config):
 
     env : MARLEnv = MARLEnv(key=init_key, multi_agent_config=MultiAgentConfig(number_of_agents_per_type=config["NUM_AGENTS_PER_TYPE"]))
     if config["CALC_EVAL"]:
-        eval_env: MARLEnv = MARLEnv(key=init_key,multi_agent_config=MultiAgentConfig(number_of_agents_per_type=config["NUM_AGENTS_PER_TYPE"],world_config=World_EnvironmentConfig(timePeriod="2024_Eval")))
+        eval_env: MARLEnv = MARLEnv(key=init_key,multi_agent_config=MultiAgentConfig(number_of_agents_per_type=config["NUM_AGENTS_PER_TYPE"],world_config=World_EnvironmentConfig(timePeriod=config["EvalTimePeriod"])))
 
     config["NUM_ACTORS_PERTYPE"] = [n * config["NUM_ENVS"] for n in config["NUM_AGENTS_PER_TYPE"]]  # Should be a list.
     config["NUM_ACTORS_TOTAL"] = env.num_agents * config["NUM_ENVS"]
@@ -171,7 +174,7 @@ def make_train(config):
         )
         return lr * frac
 
-    def train(rng):
+    def train(rng, run: wandb.sdk.wandb_run.Run = None):
         # INIT NETWORK
 
 
@@ -180,7 +183,6 @@ def make_train(config):
 
         # The outputs that depends on these and are kept seperate are;
         # - network, init_x, init_hstate, network_params, train_state
-        networks = []
         hstates = []
         network_params_list = []
         train_states = []
@@ -219,7 +221,6 @@ def make_train(config):
             init_hstate = ScannedRNN.initialize_carry(config["NUM_ACTORS_PERTYPE"][i], config["GRU_HIDDEN_DIM"])
 
             # Instead of appending dicts, maintain separate lists for each attribute
-            networks.append(network)
             hstates.append(init_hstate)
             network_params_list.append(network_params)
             train_states.append(train_state)
@@ -258,7 +259,7 @@ def make_train(config):
                 values=[]
                 log_probs=[]
 
-                for i, network in enumerate(networks):
+                for i, train_state in enumerate(train_states):
                     obs_i= last_obs[i]
                     obs_i=batchify(obs_i,config["NUM_ACTORS_PERTYPE"][i])  # Reshape to match the input shape of the network
                     ac_in = (
@@ -266,7 +267,7 @@ def make_train(config):
                         last_done[i][jnp.newaxis, :],
                         # avail_actions,
                     )
-                    h_states[i], pi, value = network.apply(train_states[i].params, h_states[i], ac_in)
+                    h_states[i], pi, value = train_state.apply_fn(train_state.params, h_states[i], ac_in)
                     values.append(value)
                     action = pi.sample(seed=_rng)
                     log_probs.append(pi.log_prob(action))
@@ -288,14 +289,14 @@ def make_train(config):
                 
                 done_batch=done
                 transitions=[]
-                for i,network in enumerate(networks):
+                for i,train_state in enumerate(train_states):
                     done_batch['agents'][i] = batchify(done["agents"][i],config["NUM_ACTORS_PERTYPE"][i]).squeeze()
                     obs_batch = batchify(obsv[i],config["NUM_ACTORS_PERTYPE"][i])
                     action_batch = batchify(actions[i],config["NUM_ACTORS_PERTYPE"][i])
                     value = values[i]
                     log_prob = log_probs[i]
 
-                    info_i={"world":info["world"],"agent":jax.tree.map(lambda x: x.reshape(config["NUM_ACTORS_PERTYPE"][i]),info["agents"][i])}
+                    info_i={"world":info["world"],"agent":jax.tree.map(lambda x: x.reshape(config["NUM_ACTORS_PERTYPE"][i],-1),info["agents"][i])}
                     # print(f"info for agenttype {i}:", info_i)
 
 
@@ -310,17 +311,13 @@ def make_train(config):
                         info_i,
                         # avail_actions,
                     ))
-                runner_state = (train_states, env_state, obsv, done_batch['agents'], hstates, rng)
+                runner_state = (train_states, env_state, obsv, done_batch['agents'], h_states, rng)
                 return runner_state, transitions
 
             initial_hstates = runner_state[-2]
             runner_state, traj_batch = jax.lax.scan(
                 _env_step, runner_state, None, config["NUM_STEPS"]
             )
-
-
-
-            
 
             # CALCULATE ADVANTAGE
             train_states, env_state, last_obs, last_dones, hstates_new, rng = runner_state
@@ -351,7 +348,7 @@ def make_train(config):
 
             advantages=[]
             targets=[]
-            for i, network in enumerate(networks):
+            for i, train_state in enumerate(train_states):
                 last_obs_batch = batchify(last_obs[i], config["NUM_ACTORS_PERTYPE"][i])
                 # avail_actions = jnp.ones(
                 #     (config["NUM_ACTORS"], env.action_space(env.agents[0]).n)
@@ -361,7 +358,7 @@ def make_train(config):
                     last_dones[i][jnp.newaxis, :],
                     # avail_actions,
                 )
-                _, _, last_val = network.apply(train_states[i].params, hstates_new[i], ac_in)
+                _, _, last_val = train_state.apply_fn(train_state.params, hstates_new[i], ac_in)
                 last_val = last_val.squeeze()
 
                 advantages_i, targets_i = _calculate_gae(config["GAMMA"][i],config["GAE_LAMBDA"][i],traj_batch[i], last_val)
@@ -370,14 +367,14 @@ def make_train(config):
 
             # UPDATE NETWORKS
             loss_infos = []
-            for i, network in enumerate(networks):
+            for i, train_state in enumerate(train_states):
                 def _update_epoch(update_state, unused):
                     def _update_minbatch(train_state, batch_info):
                         init_hstate, traj_batch, advantages, targets = batch_info
 
                         def _loss_fn(params, init_hstate, traj_batch, gae, targets):
                             # RERUN NETWORK
-                            _, pi, value = network.apply(
+                            _, pi, value = train_state.apply_fn(
                                 params,
                                 init_hstate.squeeze(),
                                 (traj_batch.obs, traj_batch.done),
@@ -481,7 +478,7 @@ def make_train(config):
                     return update_state, total_loss
 
                 update_state = (
-                    train_states[i],
+                    train_state,
                     initial_hstates[i],
                     traj_batch[i],
                     advantages[i],
@@ -493,7 +490,6 @@ def make_train(config):
                 )
                 train_states[i] = update_state[0]
                 loss_infos.append(loss_info)
-
 
 
             metrics= {}
@@ -523,17 +519,16 @@ def make_train(config):
             metrics["traj_batch"] = traj_batch
 
 
-
             if config["CALC_EVAL"]:
                 def _eval_step(eval_runner_state, unused):
-                    train_states, eval_env_state, last_obs, last_done,hstates, rng = eval_runner_state
+                    train_states, eval_env_state, last_obs, last_done,h_states, rng = eval_runner_state
                     rng, _rng = jax.random.split(rng)
                 
                     actions=[]
                     values=[]
                     log_probs=[]
 
-                    for i, network in enumerate(networks):
+                    for i, train_state in enumerate(train_states):
                         obs_i= last_obs[i]
                         obs_i=batchify(obs_i,config["NUM_ACTORS_PERTYPE"][i])  # Reshape to match the input shape of the network
                         ac_in = (
@@ -541,7 +536,7 @@ def make_train(config):
                             last_done[i][jnp.newaxis, :],
                             # avail_actions,
                         )
-                        hstates[i], pi, value = network.apply(train_states[i].params, hstates[i], ac_in)
+                        h_states[i], pi, value = train_state.apply_fn(train_state.params, h_states[i], ac_in)
                         values.append(value)
                         action = pi.sample(seed=_rng)
                         log_probs.append(pi.log_prob(action))
@@ -566,14 +561,14 @@ def make_train(config):
                     done_batch=done
                     transitions=[]    
 
-                    for i,network in enumerate(networks):
+                    for i, train_state in enumerate(train_states):
                         done_batch['agents'][i] = batchify(done["agents"][i],config["NUM_ACTORS_PERTYPE"][i]).squeeze()
                         obs_batch = batchify(obsv[i],config["NUM_ACTORS_PERTYPE"][i])
                         action_batch = batchify(actions[i],config["NUM_ACTORS_PERTYPE"][i])
                         value = values[i]
                         log_prob = log_probs[i]
 
-                        info_i={"world":info["world"],"agent":jax.tree.map(lambda x: x.reshape(config["NUM_ACTORS_PERTYPE"][i]),info["agents"][i])}
+                        info_i={"world":info["world"],"agent":jax.tree.map(lambda x: x.reshape(config["NUM_ACTORS_PERTYPE"][i],-1),info["agents"][i])}
                         # print(f"info for agenttype {i}:", info_i)
 
 
@@ -588,7 +583,7 @@ def make_train(config):
                             info_i,
                             # avail_actions,
                         ))
-                    eval_runner_state = (train_states, eval_env_state, obsv, done_batch['agents'], hstates, rng)
+                    eval_runner_state = (train_states, eval_env_state, obsv, done_batch['agents'], h_states, rng)
                     return eval_runner_state, transitions
 
                 rng, _rng = jax.random.split(rng)
@@ -598,7 +593,7 @@ def make_train(config):
 
                 eval_hstates=[]
                 init_dones_agents_eval=[]
-                for i,network in enumerate(networks):
+                for i, train_state in enumerate(train_states):
                     eval_hstates.append(ScannedRNN.initialize_carry(config["NUM_ACTORS_PERTYPE"][i], config["GRU_HIDDEN_DIM"]))
                     init_dones_agents_eval.append(jnp.zeros((config["NUM_ACTORS_PERTYPE"][i]), dtype=bool))
 
@@ -659,8 +654,9 @@ def make_train(config):
             update_steps = update_steps + 1
             runner_state = (train_states, env_state, last_obs, last_dones, hstates_new, rng)
 
+            print("Finished compiling")
             # jax.profiler.save_device_memory_profile(f"memory_{update_steps}.prof")
-            return (runner_state, update_steps), {}
+            return (runner_state, update_steps), metrics
 
         rng, _rng = jax.random.split(rng)
         runner_state = (
@@ -672,9 +668,13 @@ def make_train(config):
             _rng,
         )
 
-        jitted_update_step = jax.jit(_update_step,)
-
-
+        jitted_update_step = jax.jit(_update_step)
+        
+        orbax_checkpointer = oxcp.PyTreeCheckpointer()
+        options = oxcp.CheckpointManagerOptions(max_to_keep=2, create=True,keep_period=config["NUM_UPDATES"]//2)
+        checkpoint_manager = oxcp.CheckpointManager(
+             f'/home/myuser/data/checkpoints/MARLCheckpoints/{config["PROJECT"]}/{(run.name if run.name else run.id) if run else "GENERIC_RUN"}', orbax_checkpointer, options
+                )
 
         updates=0
         for i in range(config["NUM_UPDATES"]):
@@ -686,10 +686,23 @@ def make_train(config):
             if i>2 and i<4:
                 jax.block_until_ready((runner_state,updates,metrics))
                 jax.profiler.stop_trace()
+            print(f"Update step {updates} completed with metrics {metrics['avg_reward']}")
+            ckpt = {
+                'model': runner_state[0],  # train_states
+                # 'config': config if isinstance(config, dict) else config.as_dict(),
+                'metrics': {
+                    'train_rewards': metrics["avg_reward"],
+                    'eval_rewards': metrics["avg_reward_eval"],
+                    }
+            }
+            print(f"Saving checkpoint {updates} with metrics {metrics['avg_reward']}")
+            save_args = orbax_utils.save_args_from_target(ckpt)
+            checkpoint_manager.save(updates, ckpt, save_kwargs={"save_args": save_args})
             del metrics
             gc.collect()
         
 
+        checkpoint_manager.wait_until_finished()
 
         # runner_state, metrics = jax.lax.scan(
         #     _update_step, (runner_state, 0), None, config["NUM_UPDATES"]
@@ -739,7 +752,7 @@ def main(config):
 
 
         train_fun = make_train(wandb.config)
-        out = train_fun(rng)
+        out = train_fun(rng,run)
         # train_state = out['runner_state'][0] # runner_state.train_state
         # params = train_state.params
 
